34c5507028e31b9baa3ecdd81dd1af2e93e5e84c
李林峰 <yunlelongzai@qq.com>
2019-03-26 15:19:19 +0800

    小调整

--- sword-laboratory/src/main/go/FileService.go
+++ sword-laboratory/src/main/go/FileService.go
@@ -8,10 +8,9 @@ import (
 
 func main() {
 	_ = os.Mkdir("file", os.ModePerm)
-	http.Handle("/pollux/", http.StripPrefix("/pollux/", http.FileServer(http.Dir("file"))))
+	http.Handle("/file/", http.StripPrefix("/file/", http.FileServer(http.Dir("file"))))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-
 }
