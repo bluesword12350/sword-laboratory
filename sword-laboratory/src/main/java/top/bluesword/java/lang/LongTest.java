7b5836c08cc10a89bb9acc5ed842c4dca9d43f48
李林峰 <yunlelongzai@qq.com>
2019-04-18 14:19:54 +0800

    Long测试 依赖调整

--- sword-laboratory/src/main/java/top/bluesword/java/lang/LongTest.java
+++ sword-laboratory/src/main/java/top/bluesword/java/lang/LongTest.java
@@ -1,9 +1,18 @@
 package top.bluesword.java.lang;
 
-public class LongTest {
-	public static void main(String[] args) {
+import org.junit.jupiter.api.Test;
+
+class LongTest {
+
+	@Test
+	void parseLong() {
 		long parseInt = Long.parseLong("AB",16);
 		String string = Long.toString(parseInt, 36);
 		System.out.println(string);
+	}
+
+	@Test
+	void valueOf(){
+		System.out.println(Long.valueOf("4"));
 	}
 }
