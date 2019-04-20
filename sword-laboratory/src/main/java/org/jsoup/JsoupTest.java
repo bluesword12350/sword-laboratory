89d023792d627711472c76558305d77e93692e37
李林峰 <yunlelongzai@qq.com>
2019-04-20 17:09:02 +0800

    pom调整

--- sword-laboratory/src/main/java/org/jsoup/JsoupTest.java
+++ sword-laboratory/src/main/java/org/jsoup/JsoupTest.java
@@ -4,11 +4,9 @@ import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
 import top.bluesword.util.network.HttpClientUtil;
 
-import java.io.IOException;
-
 class JsoupTest {
     @Test
-    void parseTest() throws IOException {
+    void parseTest() {
         String html = HttpClientUtil.sendGet("https://www.baidu.com/");
         Document document = Jsoup.parse(html);
         System.out.println(document.body().select("form").get(0));
