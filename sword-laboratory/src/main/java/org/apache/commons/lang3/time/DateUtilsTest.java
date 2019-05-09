e2bc50214136fe5a5a0bd8a47173221851911c89
李林峰 <yunlelongzai@qq.com>
2019-05-09 18:13:10 +0800

    org.apache.http.client.utils.DateUtils 测试

--- sword-laboratory/src/main/java/org/apache/commons/lang3/time/DateUtilsTest.java
+++ sword-laboratory/src/main/java/org/apache/commons/lang3/time/DateUtilsTest.java
@@ -9,6 +9,10 @@ class DateUtilsTest {
 
 	@Test
 	void parse() throws ParseException {
+
+		Date date1 = DateUtils.parseDate("2019-04-08 08:00:00.0", "yyyy-MM-dd HH:mm:ss.S","yyyy-MM-dd");
+		System.out.println(date1);
+
 		Date date = DateUtils.parseDate("2018-12-25T02:29:32.858Z", "yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
 		System.out.println(DateFormatUtils.ISO_8601_EXTENDED_DATETIME_TIME_ZONE_FORMAT.format(date));
 	}
