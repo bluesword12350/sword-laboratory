b026f247a52bf5d372d35ffbd40b5fbf70ca8972
李林峰 <lilinfeng@jusdascm.com>
2019-07-30 17:07:55 +0800

    货币代码枚举测试

---
+++ sword-laboratory/src/main/java/top/bluesword/enums/CurrencyCodeEnum.java
@@ -0,0 +1,48 @@
+package top.bluesword.enums;
+
+/**
+ * 货币代码枚举
+ * 参考 ISO-4217(https://www.iso.org/iso-4217-currency-codes.html)(2019年7月29日15:21:18)
+ * @author 李林峰
+ */
+public enum CurrencyCodeEnum {
+    /**
+     * 货币代码
+     */
+    AED,AFN,ALL,AMD,ANG,
+    AOA,ARS,AUD,AWG,AZN,
+    BAM,BBD,BDT,BGN,BHD,
+    BIF,BMD,BND,BOB,BOV,
+    BRL,BSD,BTN,BWP,BYN,
+    BZD,CAD,CDF,CHE,CHF,
+    CHW,CLF,CLP,CNY,COP,
+    COU,CRC,CUC,CUP,CVE,
+    CZK,DJF,DKK,DOP,DZD,
+    EGP,ERN,ETB,EUR,FJD,
+    FKP,GBP,GEL,GHS,GIP,
+    GMD,GNF,GTQ,GYD,HKD,
+    HNL,HRK,HTG,HUF,IDR,
+    ILS,INR,IQD,IRR,ISK,
+    JMD,JOD,JPY,KES,KGS,
+    KHR,KMF,KPW,KRW,KWD,
+    KYD,KZT,LAK,LBP,LKR,
+    LRD,LSL,LYD,MAD,MDL,
+    MGA,MKD,MMK,MNT,MOP,
+    MRU,MUR,MVR,MWK,MXN,
+    MXV,MYR,MZN,NAD,NGN,
+    NIO,NOK,NPR,NZD,OMR,
+    PAB,PEN,PGK,PHP,PKR,
+    PLN,PYG,QAR,RON,RSD,
+    RUB,RWF,SAR,SBD,SCR,
+    SDG,SEK,SGD,SHP,SLL,
+    SOS,SRD,SSP,STN,SVC,
+    SYP,SZL,THB,TJS,TMT,
+    TND,TOP,TRY,TTD,TWD,
+    TZS,UAH,UGX,USD,USN,
+    UYI,UYU,UYW,UZS,VES,
+    VND,VUV,WST,XAF,XAG,
+    XAU,XBA,XBB,XBC,XBD,
+    XCD,XDR,XOF,XPD,XPF,
+    XPT,XSU,XTS,XUA,XXX,
+    YER,ZAR,ZMW,ZWL
+}
