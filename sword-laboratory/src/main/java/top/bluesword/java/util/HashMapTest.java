026ce4307c3fb73274cf797080ea08d27c74b156
李林峰 <lilinfeng@jusdascm.com>
2019-09-03 13:37:01 +0800

    map values 测试

--- sword-laboratory/src/main/java/top/bluesword/java/util/HashMapTest.java
+++ sword-laboratory/src/main/java/top/bluesword/java/util/HashMapTest.java
@@ -1,16 +1,14 @@
 package top.bluesword.java.util;
 
 import org.junit.jupiter.api.Test;
-import top.bluesword.java.grammar.NewTest;
 
 import java.util.HashMap;
-import java.util.Map;
 
 class HashMapTest {
 
 	@Test
-	void newTest(){
-		System.out.println(new HashMap<>() {{
+	void construct(){
+		System.out.println(new HashMap<>(1) {{
 			put(1, 2);
 		}});
 	}
