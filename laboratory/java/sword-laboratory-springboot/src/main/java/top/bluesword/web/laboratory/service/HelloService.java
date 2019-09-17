950b9dc42ebd520e91ac4690fa6fa56571194d51
李林峰 <lilinfeng@jusdascm.com>
2019-09-17 09:53:57 +0800

    启动服务

---
+++ laboratory/java/sword-laboratory-springboot/src/main/java/top/bluesword/web/laboratory/service/HelloService.java
@@ -0,0 +1,14 @@
+package top.bluesword.web.laboratory.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 李林峰
+ */
+@Service
+public class HelloService {
+
+    public void startUp(){
+        System.out.println("成功启动");
+    }
+}
