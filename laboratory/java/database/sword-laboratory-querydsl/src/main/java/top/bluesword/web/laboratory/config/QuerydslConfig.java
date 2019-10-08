f10d090ee6a21d1718f445d0b92f845d0738331a
李林峰 <lilinfeng@jusdascm.com>
2019-10-08 11:08:23 +0800

    基本环境建立

---
+++ laboratory/java/database/sword-laboratory-querydsl/src/main/java/top/bluesword/web/laboratory/config/QuerydslConfig.java
@@ -0,0 +1,20 @@
+package top.bluesword.web.laboratory.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+/**
+ * @author 李林峰
+ */
+@Configuration
+public class QuerydslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQuery(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
