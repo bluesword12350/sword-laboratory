78ad286c99df73c12e5d9bdc685bf49b16b2bff8
李林峰 <lilinfeng@jusdascm.com>
2019-10-10 14:13:58 +0800

    日期差值计算工具测试

---
+++ sword-laboratory/src/main/java/top/bluesword/java/time/temporal/ChronoUnitTest.java
@@ -0,0 +1,18 @@
+package top.bluesword.java.time.temporal;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+class ChronoUnitTest {
+
+    @Test
+    void between() throws ParseException {
+        Date date1 = DateUtils.parseDate("2019-04-08 08:00:00", "yyyy-MM-dd HH:mm:ss","yyyy-MM-dd");
+        Date date2 = DateUtils.parseDate("2019-04-09 08:00:00", "yyyy-MM-dd HH:mm:ss","yyyy-MM-dd");
+        System.out.println(ChronoUnit.HOURS.between(date2.toInstant(), date1.toInstant()));
+    }
+}
