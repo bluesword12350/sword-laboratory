ac59c9306c317b4c4af759f1f2e233496377dc74
李林峰 <lilinfeng@jusdascm.com>
2019-10-21 10:01:16 +0800

    htmlToExcel测试

--- laboratory/java/sword-laboratory-easypoi/src/main/java/top/bluesword/web/laboratory/controller/HtmlController.java
+++ laboratory/java/sword-laboratory-easypoi/src/main/java/top/bluesword/web/laboratory/controller/HtmlController.java
@@ -4,14 +4,17 @@ import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.ExcelXorHtmlUtil;
 import cn.afterturn.easypoi.excel.entity.ExcelToHtmlParams;
 import cn.afterturn.easypoi.excel.entity.TemplateExportParams;
+import cn.afterturn.easypoi.excel.entity.enmus.ExcelType;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import top.bluesword.web.laboratory.bean.DataGenerate;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 /**
@@ -21,13 +24,24 @@ import java.util.Map;
 @RequestMapping("html")
 public class HtmlController {
 
-    @GetMapping("toHtml")
+    @GetMapping("excelToHtml")
     public void toHtml(HttpServletResponse response) throws IOException {
         Map<String, Object> map = DataGenerate.generateMap();
-        response.setHeader("content-disposition", "attachment;filename=" + "测试文件.xlsx");
         TemplateExportParams params = new TemplateExportParams("doc/test.xlsx");
         Workbook workbook = ExcelExportUtil.exportExcel(params, map);
         response.getOutputStream().write(ExcelXorHtmlUtil.excelToHtml(new ExcelToHtmlParams(workbook)).getBytes());
+    }
+
+    @GetMapping("htmlToExcel")
+    public void htmlToExcel(HttpServletResponse response) {
+        try(InputStream resourceAsStream = getClass().getResourceAsStream("/html/sample.html");
+            Workbook workbook = ExcelXorHtmlUtil.htmlToExcel(resourceAsStream, ExcelType.XSSF);
+            ServletOutputStream outputStream = response.getOutputStream()) {
+            response.setHeader("content-disposition", "attachment;filename=test.xlsx");
+            workbook.write(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
