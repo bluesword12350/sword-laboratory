ca4b0d001b919ee1fd60a45b0a8ca65537845e24
李林峰 <lilinfeng@jusdascm.com>
2019-10-22 11:12:50 +0800

    thymeleaf 日期格式化测试完成

--- laboratory/java/sword-laboratory-easypoi/src/main/java/top/bluesword/web/laboratory/controller/HtmlController.java
+++ laboratory/java/sword-laboratory-easypoi/src/main/java/top/bluesword/web/laboratory/controller/HtmlController.java
@@ -13,11 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import top.bluesword.web.laboratory.bean.DataGenerate;
+import top.bluesword.web.laboratory.bean.User;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -54,8 +56,8 @@ public class HtmlController {
     @GetMapping("htmlToExcelByThymeleaf")
     public void htmlToExcelByThymeleaf(HttpServletResponse response) {
         Context context = new Context();
-        List<String> users = List.of("user1", "user5");
-        context.setVariable("usernameList", users);
+        List<User> users = getUsers();
+        context.setVariable("users", users);
         String result = templateEngine.process("excelTemplate", context);
         try(Workbook workbook = ExcelXorHtmlUtil.htmlToExcel(result, ExcelType.XSSF);
             ServletOutputStream outputStream = response.getOutputStream()) {
@@ -64,6 +66,15 @@ public class HtmlController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private List<User> getUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(User.builder().name("u1").level("super admin").build());
+        users.add(User.builder().name("u2").level("admin").build());
+        users.add(User.builder().name("u3").build());
+        users.add(User.builder().name("u4").level("member").createDate(System.currentTimeMillis()).build());
+        return users;
     }
 
 }
