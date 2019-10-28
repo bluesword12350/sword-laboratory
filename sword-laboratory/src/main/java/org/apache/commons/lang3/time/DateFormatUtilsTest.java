b4f6c5b36fbac5c74781be28c6a42d7b30f0eef5
李林峰 <lilinfeng@jusdascm.com>
2019-10-28 16:07:27 +0800

    日期格式测试

--- sword-laboratory/src/main/java/org/apache/commons/lang3/time/DateFormatUtilsTest.java
+++ sword-laboratory/src/main/java/org/apache/commons/lang3/time/DateFormatUtilsTest.java
@@ -13,5 +13,11 @@ class DateFormatUtilsTest {
 		System.out.println(DateFormatUtils.format(date, "yyyy-MM-dd HH:mm:ss.SSS ZZ"));
         System.out.println(DateFormatUtils.format(date, "yyyy-MM-dd HH:mm:ss.SSS ZZ",TimeZone.getTimeZone("UCT")));
 		System.out.println(DateFormatUtils.format(date, "('UTC' X) yyyy/MM/dd HH:mm"));
+		System.out.println(DateFormatUtils.format(date, "('UTC' XX) yyyy/MM/dd HH:mm"));
+		System.out.println(DateFormatUtils.format(date, "('UTC' XXX) yyyy/MM/dd HH:mm"));
+
+		System.out.println(DateFormatUtils.format(date, "'UTC'(z) yyyy-MM-dd HH:mm:ss"));
+		System.out.println(DateFormatUtils.format(date, "'UTC'(Z) yyyy-MM-dd HH:mm:ss"));
+		System.out.println(DateFormatUtils.format(date, "'UTC'(ZZ) yyyy-MM-dd HH:mm:ss"));
 	}
 }
