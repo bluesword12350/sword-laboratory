dffcbfe94d5d02440720d0e0232ece47a83e2ffe
李林峰 <yunlelongzai@qq.com>
2020-03-17 15:25:26 +0800

    LinkedList 测试

--- sword-laboratory/src/main/java/top/bluesword/java/util/LinkedListTest.java
+++ sword-laboratory/src/main/java/top/bluesword/java/util/LinkedListTest.java
@@ -11,9 +11,33 @@ class LinkedListTest {
 		LinkedList<Integer> linkedList = new LinkedList<>();
 		linkedList.add(1);
 		linkedList.add(2);
+		linkedList.add(null);
 		System.out.println(linkedList);
 		
 		linkedList.add(1, 3);
 		System.out.println(linkedList);
 	}
+
+	@Test
+	void peek(){
+		LinkedList<Integer> linkedList = new LinkedList<>();
+		linkedList.add(1);
+		linkedList.add(2);
+
+		Integer peek = linkedList.peek();
+		System.out.println(peek);
+		System.out.println(linkedList.size());
+	}
+
+	@Test
+	void poll(){
+		LinkedList<Integer> linkedList = new LinkedList<>();
+		linkedList.add(1);
+		linkedList.add(2);
+
+		Integer poll = linkedList.poll();
+		System.out.println(poll);
+		System.out.println(linkedList.size());
+	}
+
 }
