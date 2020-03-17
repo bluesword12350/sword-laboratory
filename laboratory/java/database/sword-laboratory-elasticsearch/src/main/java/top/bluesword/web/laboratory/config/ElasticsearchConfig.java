657cda7c3d94b5859cbe1c7dde67a86ba7d41c1c
李林峰 <yunlelongzai@qq.com>
2020-03-17 15:23:29 +0800

    删除多余代码

--- laboratory/java/database/sword-laboratory-elasticsearch/src/main/java/top/bluesword/web/laboratory/config/ElasticsearchConfig.java
+++ laboratory/java/database/sword-laboratory-elasticsearch/src/main/java/top/bluesword/web/laboratory/config/ElasticsearchConfig.java
@@ -1,22 +1,15 @@
 package top.bluesword.web.laboratory.config;
 
-import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
-import org.springframework.data.elasticsearch.core.ResultsMapper;
-import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
 import org.springframework.data.elasticsearch.repository.support.ElasticsearchRepositoryFactory;
 
+/**
+ * @author 李林峰
+ */
 @Configuration
 public class ElasticsearchConfig {
-
-//    @Bean
-//    public ElasticsearchOperations elasticsearchTemplate(RestHighLevelClient client, ElasticsearchConverter converter,
-//                                                           ResultsMapper resultsMapper) {
-//        return new ElasticsearchRestTemplate(client, converter, resultsMapper);
-//    }
 
     @Bean
     public ElasticsearchRepositoryFactory elasticsearchRepositoryFactory(ElasticsearchRestTemplate elasticsearchTemplate){
