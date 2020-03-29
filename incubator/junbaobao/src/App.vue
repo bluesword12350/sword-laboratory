5406b1065676cb81e5ebfa95c25a5322d0172eb1
李林峰 <yunlelongzai@qq.com>
2020-03-29 18:50:53 +0800

    君宝宝更新

--- incubator/junbaobao/src/App.vue
+++ incubator/junbaobao/src/App.vue
@@ -1,24 +1,39 @@
 <template>
   <div id="app">
-    <h1 style="text-align:center;">{{title}}</h1>
-    <a-progress :percent="percent" :strokeWidth="20" :showInfo="false" :strokeColor="red"/>
+    <h1 class="center-text">{{title}}</h1>
+    <a-progress :percent="percent" :strokeWidth="20" :showInfo="false" strokeColor="red"/>
+    <h2 class="center-text">{{konwTimeText}}</h2>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import { Progress } from 'ant-design-vue';
+import dayjs from 'dayjs'
+
+Vue.use(Progress);
+
 let lastSpringFestival = new Date(2020, 0, 25).getTime();
 let nextSpringFestival = new Date(2021, 1, 12).getTime();
 let percent = ((Date.now() - lastSpringFestival) / (nextSpringFestival - lastSpringFestival)) * 100;
+let knowDate = new Date(2020,0,23);
+let dateTime = 24*60*60*1000;
+let knowDateToNow = `${Math.trunc((Date.now()-knowDate.getTime())/dateTime)}天`
+
 export default {
   name: "App",
   data() {
     return {
-      title: "2020农历年进度:"+percent+"%",
-      percent: percent
+      title: `2020农历年进度:${percent}%`,
+      percent : percent,
+      konwTimeText : `与君宝宝结识于 ${dayjs(knowDate).format("YYYY-MM-DD")} ,过去了 ${knowDateToNow}`
     };
   }
 };
 </script>
 
 <style>
+  .center-text{
+    text-align:center;
+  }
 </style>
--- incubator/junbaobao/src/main.js
+++ incubator/junbaobao/src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import App from './App';
 import 'ant-design-vue/dist/antd.css';
-import { Progress } from 'ant-design-vue';
 
-Vue.use(Progress);
 Vue.config.productionTip = false;
 
 new Vue({
