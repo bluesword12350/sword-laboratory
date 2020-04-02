58f8aec72f90baf9a251c46528681590fcafd53a
李林峰 <yunlelongzai@qq.com>
2020-04-02 14:06:19 +0800

    xiaoymin swagger ui 测试

--- laboratory/java/document/sword-laboratory-swagger/src/main/java/top/bluesword/web/laboratory/controller/SwaggerDemoController.java
+++ laboratory/java/document/sword-laboratory-swagger/src/main/java/top/bluesword/web/laboratory/controller/SwaggerDemoController.java
@@ -3,9 +3,11 @@ package top.bluesword.web.laboratory.controller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Base64;
+import java.util.Objects;
 
 import javax.servlet.http.HttpServletResponse;
 
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-
+/**
+ * @author 李林峰
+ */
 @RestController
 @RequestMapping("SwaggerDemo")
 @Api(tags = "Swagger测试")
@@ -31,12 +32,16 @@ public class SwaggerDemoController {
 
 	@PostMapping(value = "image")
 	@ApiOperation("文件测试")
-	public String image(@ApiParam(required = true) MultipartFile file, String filename, HttpServletResponse response) {
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "file",value = "文件"),
+			@ApiImplicitParam(name = "filename",value = "文件名称")
+	})
+	public String image(@ApiParam(required = true) MultipartFile file,  String filename, HttpServletResponse response) {
 		if (file.isEmpty()) return "文件为空";
-		String[] split = file.getOriginalFilename().split("\\.");
+		String[] split = Objects.requireNonNull(file.getOriginalFilename()).split("\\.");
 		long size = file.getSize();
 		log.info("文件大小为"+(size/1024.0)+"KB");
-		if (!split[split.length-1].equals("jpg")) {
+		if (!"jpg".equals(split[split.length-1])) {
 			return "请选择JPG图片";
 		}
 		try (InputStream inputStream = file.getInputStream()) {
