ac65063be035d7acd9f1c44c40057c442468ca80
李林峰 <yunlelongzai@qq.com>
2020-04-09 15:58:55 +0800

    Tink 拆分

--- laboratory/java/encode/sword-laboratory-tink/src/test/java/com/google/crypto/tink/TinkTest.java
+++ laboratory/java/encode/sword-laboratory-tink/src/test/java/com/google/crypto/tink/TinkTest.java
@@ -1,20 +1,12 @@
 package com.google.crypto.tink;
 
 import cn.hutool.core.io.FileUtil;
-import com.google.crypto.tink.aead.AeadFactory;
 import com.google.crypto.tink.aead.AeadKeyTemplates;
 import com.google.crypto.tink.config.TinkConfig;
-import com.google.crypto.tink.daead.DeterministicAeadFactory;
 import com.google.crypto.tink.daead.DeterministicAeadKeyTemplates;
-import com.google.crypto.tink.hybrid.HybridDecryptFactory;
-import com.google.crypto.tink.hybrid.HybridEncryptFactory;
 import com.google.crypto.tink.hybrid.HybridKeyTemplates;
-import com.google.crypto.tink.mac.MacFactory;
 import com.google.crypto.tink.mac.MacKeyTemplates;
-import com.google.crypto.tink.signature.PublicKeySignFactory;
-import com.google.crypto.tink.signature.PublicKeyVerifyFactory;
 import com.google.crypto.tink.signature.SignatureKeyTemplates;
-import com.google.crypto.tink.streamingaead.StreamingAeadFactory;
 import com.google.crypto.tink.streamingaead.StreamingAeadKeyTemplates;
 import com.google.crypto.tink.subtle.Hex;
 import org.junit.jupiter.api.Test;
@@ -44,8 +36,7 @@ class TinkTest {
     void aes128Test() throws GeneralSecurityException {
         KeysetHandle keysetHandle = KeysetHandle.generateNew(
                 AeadKeyTemplates.AES128_GCM);
-        Aead aead = AeadFactory.getPrimitive(keysetHandle);
-
+        Aead aead = keysetHandle.getPrimitive(Aead.class);
         byte[] cipherText = aead.encrypt(plaintext, aad);
         System.out.println(Hex.encode(cipherText));
         byte[] decrypted = aead.decrypt(cipherText, aad);
@@ -56,10 +47,10 @@ class TinkTest {
     void aes256Test() throws GeneralSecurityException {
         KeysetHandle keysetHandle = KeysetHandle.generateNew(
                 DeterministicAeadKeyTemplates.AES256_SIV);
-        DeterministicAead dAead = DeterministicAeadFactory.getPrimitive(keysetHandle);
-        byte[] cipherText = dAead.encryptDeterministically(plaintext, aad);
+        DeterministicAead deterministicAead = keysetHandle.getPrimitive(DeterministicAead.class);
+        byte[] cipherText = deterministicAead.encryptDeterministically(plaintext, aad);
         System.out.println(Hex.encode(cipherText));
-        byte[] decrypted = dAead.decryptDeterministically(cipherText, aad);
+        byte[] decrypted = deterministicAead.decryptDeterministically(cipherText, aad);
         System.out.println(new String(decrypted));
     }
 
@@ -67,7 +58,7 @@ class TinkTest {
     void macTest() throws GeneralSecurityException {
         KeysetHandle keysetHandle = KeysetHandle.generateNew(
                 MacKeyTemplates.HMAC_SHA256_128BITTAG);
-        Mac mac = MacFactory.getPrimitive(keysetHandle);
+        Mac mac = keysetHandle.getPrimitive(Mac.class);
         byte[] tag = mac.computeMac(data);
         System.out.println(Hex.encode(tag));
         try {
@@ -86,31 +77,29 @@ class TinkTest {
 
     @Test
     void ecdsaSignTest() throws GeneralSecurityException, IOException {
-        KeysetHandle privateKeysetHandle = KeysetHandle.generateNew(
+        String publicKeyFileName = ".publicKey";
+
+        KeysetHandle privateKeySetHandle = KeysetHandle.generateNew(
                 SignatureKeyTemplates.ECDSA_P256);
-        PublicKeySign signer = PublicKeySignFactory.getPrimitive(
-                privateKeysetHandle);
+        PublicKeySign signer = privateKeySetHandle.getPrimitive(PublicKeySign.class);
         byte[] signature = signer.sign(data);
         System.out.println(Hex.encode(signature));
-        KeysetHandle publicKeysetHandle =
-                privateKeysetHandle.getPublicKeysetHandle();
+        KeysetHandle publicKeySetHandle =
+                privateKeySetHandle.getPublicKeysetHandle();
         try (FileOutputStream outputStream =
-                     new FileOutputStream("publicKey")){
+                     new FileOutputStream(publicKeyFileName)){
             BinaryKeysetWriter.withOutputStream(outputStream)
-                    .write(publicKeysetHandle.getKeyset());
+                    .write(publicKeySetHandle.getKeyset());
         }
-    }
 
-    @Test
-    void ecdsaVerifierTest() throws GeneralSecurityException, IOException {
-        KeysetHandle publicKeysetHandle = KeysetHandle.fromKeyset(BinaryKeysetReader.withFile(new File("publicKey"))
-                .read());
+        KeysetHandle publicKeySetHandleFromFile = KeysetHandle.fromKeyset(
+                BinaryKeysetReader.withFile(new File(publicKeyFileName)).read()
+        );
         String sign = "";
-        byte[] signature = Hex.decode(sign);
-        PublicKeyVerify verifier = PublicKeyVerifyFactory.getPrimitive(
-                publicKeysetHandle);
+        byte[] signature1 = Hex.decode(sign);
+        PublicKeyVerify verifier = publicKeySetHandleFromFile.getPrimitive(PublicKeyVerify.class);
         try {
-            verifier.verify(signature, data);
+            verifier.verify(signature1, data);
             System.out.println("data:验证成功");
         }catch (Exception e){
             System.out.println("data:验证失败");
@@ -119,49 +108,46 @@ class TinkTest {
 
     @Test
     void hybridEncryptTest() throws GeneralSecurityException, IOException {
-        KeysetHandle privateKeysetHandle = KeysetHandle.generateNew(
+        String fileName = ".privateKey";
+
+        KeysetHandle privateKeySetHandle = KeysetHandle.generateNew(
                 HybridKeyTemplates.ECIES_P256_HKDF_HMAC_SHA256_AES128_GCM);
-        KeysetHandle publicKeysetHandle =
-                privateKeysetHandle.getPublicKeysetHandle();
-        HybridEncrypt hybridEncrypt = HybridEncryptFactory.getPrimitive(
-                publicKeysetHandle);
+        KeysetHandle publicKeySetHandle =
+                privateKeySetHandle.getPublicKeysetHandle();
+        HybridEncrypt hybridEncrypt = publicKeySetHandle.getPrimitive(HybridEncrypt.class);
 
         byte[] cipherText = hybridEncrypt.encrypt(plaintext, contextInfo);
         System.out.println(Hex.encode(cipherText));
         try (FileOutputStream outputStream =
-                     new FileOutputStream("publicKey.json")){
+                     new FileOutputStream(fileName)){
             JsonKeysetWriter.withOutputStream(outputStream)
-                    .write(privateKeysetHandle.getKeyset());
+                    .write(privateKeySetHandle.getKeyset());
         }
-    }
 
-    @Test
-    void hybridDecryptTest() throws GeneralSecurityException, IOException {
-        KeysetHandle privateKeysetHandle = KeysetHandle.fromKeyset(
-                JsonKeysetReader.withFile(new File("publicKey.json")).read());
-        HybridDecrypt hybridDecrypt = HybridDecryptFactory.getPrimitive(
-                privateKeysetHandle);
-        String cipher = "";
-        byte[] cipherText = Hex.decode(cipher);
+        KeysetHandle privateKeySetHandleByFile = KeysetHandle.fromKeyset(
+                JsonKeysetReader.withFile(new File(fileName)).read());
+        HybridDecrypt hybridDecrypt = privateKeySetHandleByFile.getPrimitive(HybridDecrypt.class);
         System.out.println(new String(hybridDecrypt.decrypt(cipherText, contextInfo)));
     }
 
     @Test
     void streamingAeadTest () throws GeneralSecurityException, IOException {
+        String encryptedFile = "pom加密.encrypted";
+
         KeysetHandle keysetHandle = KeysetHandle.generateNew(
                 StreamingAeadKeyTemplates.AES128_CTR_HMAC_SHA256_4KB);
-        StreamingAead streamingAead = StreamingAeadFactory.getPrimitive(keysetHandle);
-        try (FileOutputStream fileOutputStream = new FileOutputStream("李白加密")){
+        StreamingAead streamingAead = keysetHandle.getPrimitive(StreamingAead.class);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(encryptedFile)){
             FileChannel cipherTextDestination = fileOutputStream.getChannel();
             try (WritableByteChannel encryptingChannel =
                          streamingAead.newEncryptingChannel(cipherTextDestination, aad)){
-                byte[] bytes = FileUtil.readBytes(new File("李白.json"));
+                byte[] bytes = FileUtil.readBytes(new File("pom.xml"));
                 ByteBuffer buffer = ByteBuffer.wrap(bytes);
                 encryptingChannel.write(buffer);
             }
         }
-        FileInputStream cipherTextSource = new FileInputStream("李白加密");
+        FileInputStream cipherTextSource = new FileInputStream(encryptedFile);
         InputStream inputStream1 = streamingAead.newDecryptingStream(cipherTextSource, aad);
-        FileUtil.writeFromStream(inputStream1,new File("李白2.json"));
+        FileUtil.writeFromStream(inputStream1,new File("pom.iml"));
     }
 }
