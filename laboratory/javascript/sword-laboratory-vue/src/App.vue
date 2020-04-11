7d3e7c4df38ca3e08d7f8dbfc119ebe665e06c3f
李林峰 <yunlelongzai@qq.com>
2020-04-11 10:17:01 +0800

    vue 使用

---
+++ laboratory/javascript/sword-laboratory-vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Timestamp/>
+  </div>
+</template>
+
+<script>
+import Timestamp from "./component/Timestamp";
+
+export default {
+  name: "App",
+  components: {
+    Timestamp
+  },
+};
+</script>
+
+<style>
+</style>
