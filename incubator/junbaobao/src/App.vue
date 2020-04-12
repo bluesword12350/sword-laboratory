ef92fe78ba2b5cec8efd046a5691ca4f06d5dc04
李林峰 <yunlelongzai@qq.com>
2020-04-12 09:11:37 +0800

    移除时间进度条

--- incubator/junbaobao/src/App.vue
+++ incubator/junbaobao/src/App.vue
@@ -3,12 +3,6 @@
     <h1 class="center-text">{{`2020农历年进度:${percent}%`}}</h1>
     <a-progress :percent="percentNum" :strokeWidth="20" :showInfo="false" strokeColor="red"/>
     <h2 class="center-text">与君宝宝结识于 {{knowDateTxt}} ,过去了 {{knowDateToNow}} 天</h2>
-    <h3 class="center-text">今天的进度</h3>
-    <a-progress :percent="dayPercent" :strokeWidth="10" :showInfo="false" strokeColor="blue"/>
-    <h3 class="center-text">小时</h3>
-    <a-progress :percent="hourPercent" :strokeWidth="10" :showInfo="false" strokeColor="blue"/>
-    <h3 class="center-text">分钟</h3>
-    <a-progress :percent="minutePercent" :strokeWidth="10" :showInfo="false" strokeColor="blue"/>
   </div>
 </template>
 
@@ -16,7 +10,6 @@
 import Vue from 'vue';
 import { Progress } from 'ant-design-vue';
 import dayjs from 'dayjs'
-import "./javascript/DateEnhance"
 
 Vue.use(Progress);
 
@@ -31,9 +24,6 @@ export default {
     return {
       percent : 0,
       percentNum : 0,
-      dayPercent : 0,
-      hourPercent : 0,
-      minutePercent : 0,
       knowDateToNow : Math.trunc((Date.now()-knowDate.getTime()) / dateTime),
       knowDateTxt : dayjs(knowDate).format("YYYY-MM-DD")
     };
@@ -43,22 +33,10 @@ export default {
       let percent = ((Date.now() - lastSpringFestival) / (nextSpringFestival - lastSpringFestival));
       this.percent =  (percent * 100).toFixed(6);
       this.percentNum = Number(this.percent)
-    },
-    pushDayPercent(){
-      this.dayPercent = new Date().getMillisecondsOfDay()/(24*60*60*1000/100);
-    },
-    pushHoursPercent(){
-      this.hourPercent = new Date().getMillisecondsOfHour()/(60*60*1000/100);
-    },
-    pushMinutePercent(){
-      this.minutePercent = new Date().getMillisecondsOfMinute()/(60*1000/100);
     }
   },
   mounted(){
     setInterval(this.pushPercent,100);
-    setInterval(this.pushDayPercent,100);
-    setInterval(this.pushHoursPercent,100);
-    setInterval(this.pushMinutePercent,100);
   }
 };
 </script>
