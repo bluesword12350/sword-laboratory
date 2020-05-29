b5d075922de5ba796b48f0b7431d47bd892d36f8
李林峰 <yunlelongzai@qq.com>
2020-05-29 16:51:24 +0800

    日期类测试

---
+++ sword-laboratory/src/test/java/top/bluesword/java/time/LocalDateTest.java
@@ -0,0 +1,16 @@
+package top.bluesword.java.time;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class LocalDateTest {
+
+    @Test
+    void test(){
+        LocalDate now = LocalDate.now();
+        System.out.println(now.plus(1, ChronoUnit.DAYS));
+    }
+
+}
