45c587d29b9a8ac2560ebff9a7e6896af338308c
李林峰 <yunlelongzai@qq.com>
2020-06-05 08:58:53 +0800

    功能补充

--- laboratory/java/util/sword-laboratory-hibernatevalidator/src/main/java/top/bluesword/laboratory/bean/InsideBeanDemo.java
+++ laboratory/java/util/sword-laboratory-hibernatevalidator/src/main/java/top/bluesword/laboratory/bean/InsideBeanDemo.java
@@ -1,11 +1,14 @@
 package top.bluesword.laboratory.bean;
 
+import lombok.Data;
+
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 
 /**
  * @author 李林峰
  */
+@Data
 public class InsideBeanDemo {
 
 	@Max(value = 100,message = "太大了")
