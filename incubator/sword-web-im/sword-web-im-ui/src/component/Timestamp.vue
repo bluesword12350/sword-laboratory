b6351e447c4749edc29989ea757b75a4b2357487
李林峰 <yunlelongzai@qq.com>
2020-07-06 14:47:13 +0800

    web im ui

---
+++ incubator/sword-web-im/sword-web-im-ui/src/component/Timestamp.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1 class="center-text">{{`${now} 毫秒`}}</h1>
+</template>
+
+<script>
+    export default {
+        name: "Timestamp",
+        data() {
+            return {
+                now : Date.now(),
+            };
+        },
+        methods:{
+            setNow(){
+                this.now = Date.now();
+            }
+        },
+        mounted(){
+            setInterval(this.setNow,1);
+        }
+    }
+</script>
+
+<style scoped>
+    .center-text{
+        text-align:center;
+    }
+</style>
