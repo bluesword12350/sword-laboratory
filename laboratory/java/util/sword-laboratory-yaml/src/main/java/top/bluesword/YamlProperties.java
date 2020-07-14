68f45ec83b7ef32d1c56f731782ca99dd855319a
李林峰 <yunlelongzai@qq.com>
2020-07-14 14:00:41 +0800

    yaml map 测试

--- laboratory/java/util/sword-laboratory-yaml/src/main/java/top/bluesword/YamlProperties.java
+++ laboratory/java/util/sword-laboratory-yaml/src/main/java/top/bluesword/YamlProperties.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author 李林峰
@@ -17,5 +18,7 @@ import java.util.List;
 public class YamlProperties {
 
     private List<String> ls;
+
+    private Map<String,List<String>> map;
 
 }
--- laboratory/java/util/sword-laboratory-yaml/src/test/java/top/bluesword/YamlTest.java
+++ laboratory/java/util/sword-laboratory-yaml/src/test/java/top/bluesword/YamlTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 public class YamlTest {
@@ -13,8 +14,14 @@ public class YamlTest {
     YamlProperties yaml;
 
     @Test
-    void test() {
+    void ls() {
         List<String> ls = yaml.getLs();
         System.out.println(ls);
+    }
+
+    @Test
+    void map() {
+        Map<String, List<String>> map = yaml.getMap();
+        System.out.println(map);
     }
 }
