e4ce61e7d88d180bfa0cd901b8515a6c0291c7c6
李林峰 <yunlelongzai@qq.com>
2020-07-14 17:58:20 +0800

    数组并集方法

--- laboratory/sql/postgresql/src/main/sql/array.sql
+++ laboratory/sql/postgresql/src/main/sql/array.sql
@@ -3,3 +3,11 @@ SELECT array_agg(num) FROM (VALUES (array[1,2]), (array[3,4]), (array[5,6])) AS 
 SELECT array_cat(array[[1,2],[3,4]],array[[5,6],[7,8]]);
 
 SELECT unnest(num) FROM (VALUES (array[[1,2],[3,4]]), (array[[5,6],[7,8]])) AS t (num);
+
+select 1 = any(array[1,2,3]);
+
+SELECT UNNEST(array['two', 'four', 'six'])
+INTERSECT
+SELECT UNNEST(array['four', 'six', 'eight']);
+
+SELECT ARRAY(SELECT * FROM UNNEST(ARRAY[1, 4, 2]) WHERE UNNEST = ANY(ARRAY[2, 3]));
