1391516c483c545ba6abf32a29a230e15864a0cf
李林峰 <yunlelongzai@qq.com>
2020-07-23 15:47:26 +0800

    Gson parse测试

--- laboratory/java/util/sword-laboratory-json/src/test/java/com/google/gson/GsonTest.java
+++ laboratory/java/util/sword-laboratory-json/src/test/java/com/google/gson/GsonTest.java
@@ -1,12 +1,12 @@
 package com.google.gson;
 
+import com.google.gson.reflect.TypeToken;
+import org.junit.jupiter.api.Test;
+
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
-
-import com.google.gson.reflect.TypeToken;
-import org.junit.jupiter.api.Test;
 
 class GsonTest {
 
@@ -37,9 +37,20 @@ class GsonTest {
 		System.out.println(json);
 		GsonBeanDemo gsonBeanDemoFromJson = gson.fromJson(json, GsonBeanDemo.class);
 		System.out.println(gsonBeanDemoFromJson);
-
 	}
-	
+
+	@Test
+	void parse(){
+		Gson gson = new Gson();
+		GsonBeanDemo gsonBeanDemo = new GsonBeanDemo();
+		gsonBeanDemo.setString("123");
+		String jsonStr = gson.toJson(gsonBeanDemo);
+		JsonParser jsonParser = new JsonParser();
+		JsonElement parse = jsonParser.parse(jsonStr);
+		JsonObject asJsonObject = parse.getAsJsonObject();
+		System.out.println(asJsonObject);
+	}
+
 	public static class BeanDemo {
 		String string;
 		BigDecimal bigDecimal;
