7337e81c24414a9dbca3bdfc37058564556d2d3c
李林峰 <yunlelongzai@qq.com>
2020-07-27 09:37:18 +0800

    set顺序测试

--- sword-laboratory/src/test/java/top/bluesword/java/util/SetTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/util/SetTest.java
@@ -3,6 +3,8 @@ package top.bluesword.java.util;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 class SetTest {
@@ -12,5 +14,12 @@ class SetTest {
 		Set<String> set = new HashSet<>();
 		set.add("12");
 		System.out.println(set.contains("1"+"2"));
+	}
+
+	@Test
+	void sortTest() {
+		List<String> strings = List.of("2", "1", "3");
+		System.out.println(new HashSet<>(strings));
+		System.out.println(new LinkedHashSet<>(strings));
 	}
 }
