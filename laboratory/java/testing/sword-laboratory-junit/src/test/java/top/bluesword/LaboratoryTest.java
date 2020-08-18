08530f79e5e7a778535118d2348479c941129b3f
李林峰 <yunlelongzai@qq.com>
2020-08-18 13:12:56 +0800

    Assertions 使用测试

--- laboratory/java/testing/sword-laboratory-junit/src/test/java/top/bluesword/LaboratoryTest.java
+++ laboratory/java/testing/sword-laboratory-junit/src/test/java/top/bluesword/LaboratoryTest.java
@@ -2,11 +2,16 @@ package top.bluesword;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LaboratoryTest {
 
     @Test
     void print(){
-        System.out.println(1);
+        int anInt = new Random().nextInt(2);
+        assertTrue(anInt<2);
     }
 
 }
