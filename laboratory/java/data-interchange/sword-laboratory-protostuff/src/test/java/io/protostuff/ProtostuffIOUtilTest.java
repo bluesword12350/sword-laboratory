7e7f3e276a380c8ffc0564066f9eaaad5e0945f2
李林峰 <yunlelongzai@qq.com>
2020-08-26 11:20:36 +0800

    protostuff list 空值测试

--- laboratory/java/data-interchange/sword-laboratory-protostuff/src/test/java/io/protostuff/ProtostuffIOUtilTest.java
+++ laboratory/java/data-interchange/sword-laboratory-protostuff/src/test/java/io/protostuff/ProtostuffIOUtilTest.java
@@ -1,19 +1,23 @@
 package io.protostuff;
 
 import io.protostuff.runtime.RuntimeSchema;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import top.bluesword.bean.BeanDemo;
 import top.bluesword.bean.InsideBeanDemo;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProtostuffIOUtilTest {
 
     @Test
-    void test() {
+    void toByteArray() {
         BeanDemo beanDemo = new BeanDemo();
         beanDemo.setString("132321");
         beanDemo.setDate(ZonedDateTime.now());
@@ -29,7 +33,27 @@ class ProtostuffIOUtilTest {
         }
         BeanDemo fooParsed = schema.newMessage();
         ProtostuffIOUtil.mergeFrom(protostuff, fooParsed, schema);
-        System.out.println(fooParsed.toString());
-        Assertions.assertEquals(beanDemo,fooParsed);
+        System.out.println(fooParsed);
+        assertEquals(beanDemo,fooParsed);
+    }
+
+    @Test
+    void list() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("1");
+        stringList.add(null);
+        stringList.add("3");
+        Schema<String> schema = RuntimeSchema.getSchema(String.class);
+        MessageCollectionSchema<String> stringMessageCollectionSchema = new MessageCollectionSchema<>(schema);
+        LinkedBuffer buffer = LinkedBuffer.allocate(512);
+        final byte[] protostuff;
+        try {
+            protostuff = ProtostuffIOUtil.toByteArray(stringList, stringMessageCollectionSchema, buffer);
+        } finally {
+            buffer.clear();
+        }
+        Collection<String> strings = stringMessageCollectionSchema.newMessage();
+        ProtostuffIOUtil.mergeFrom(protostuff, strings, stringMessageCollectionSchema);
+        System.out.println(strings);
     }
 }
