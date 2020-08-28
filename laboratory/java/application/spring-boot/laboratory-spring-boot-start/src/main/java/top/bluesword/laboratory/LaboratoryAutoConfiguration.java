bce740eca6f5fdd4c05d74d7b73d9b591d944dab
李林峰 <yunlelongzai@qq.com>
2020-08-28 10:12:20 +0800

    spring-boot-starter 使用

---
+++ laboratory/java/application/spring-boot/laboratory-spring-boot-start/src/main/java/top/bluesword/laboratory/LaboratoryAutoConfiguration.java
@@ -0,0 +1,12 @@
+package top.bluesword.laboratory;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 李林峰
+ */
+@Configuration
+@ComponentScan("top.bluesword.laboratory")
+public class LaboratoryAutoConfiguration {
+}
