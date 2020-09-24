21cb83991611466699b7181ae2ac7a57d5d022d6
李林峰 <yunlelongzai@qq.com>
2020-09-24 15:46:07 +0800

    javapoet 初步使用

---
+++ laboratory/java/util/sword-laboratory-javapoet/src/test/java/com/squareup/javapoet/JavapoetTest.java
@@ -0,0 +1,33 @@
+package com.squareup.javapoet;
+
+import org.junit.jupiter.api.Test;
+
+import javax.lang.model.element.Modifier;
+import java.io.File;
+import java.io.IOException;
+
+public class JavapoetTest {
+
+    @Test
+    void test() throws IOException {
+        MethodSpec main = MethodSpec.methodBuilder("main")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .returns(void.class)
+                .addParameter(String[].class, "args")
+                .addStatement("$T.out.println($S)", System.class, "你好, JavaPoet!")
+                .build();
+
+        TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+                .addJavadoc(String.format("@author %s","李林峰"))
+                .addJavadoc("")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build();
+
+        JavaFile javaFile = JavaFile.builder("top.bluesword.laboratory", helloWorld)
+                .build();
+
+        javaFile.writeTo(new File("src/main/java"));
+    }
+
+}
