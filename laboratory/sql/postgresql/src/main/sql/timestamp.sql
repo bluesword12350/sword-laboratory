c780a78156a2801a3f4bee619aee3215c51307a6
李林峰 <yunlelongzai@qq.com>
2020-10-07 10:37:10 +0800

    postgre 日期运算

--- laboratory/sql/postgresql/src/main/sql/timestamp.sql
+++ laboratory/sql/postgresql/src/main/sql/timestamp.sql
@@ -1,6 +1,17 @@
+--https://www.postgresql.org/docs/10/functions-datetime.html#9.9.1
+
 --日期转 unix 纪元 数字时间戳
 select extract(epoch from now());
 
 --时间戳格式化为字符串
 select to_char(now(),'YYYY-MM-DD HH24:MI:SS TZ');
 select to_char(now(),'YYYY-MM-DD HH24:MI:SS OF');
+
+--当前日期
+select current_date;
+
+--日期运算
+select current_date - interval '6 month';
+
+--日期比较
+select date '2020-04-07' >= current_date - interval '6 month'
