c93d8797a9c3f544bb04981434114137a59a39c1
李林峰 <yunlelongzai@qq.com>
2020-10-14 16:49:46 +0800

    spel 测试

--- sword-laboratory/src/test/java/org/springframework/expression/spel/standard/SpelExpressionParserTest.java
+++ sword-laboratory/src/test/java/org/springframework/expression/spel/standard/SpelExpressionParserTest.java
@@ -4,19 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.expression.common.TemplateParserContext;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
-import java.util.HashMap;
 import java.util.Map;
 
 class SpelExpressionParserTest {
 
     @Test
     void parseExpression(){
-        Map<String,Object> map = new HashMap<>();
-        map.put("name","李林峰");
         StandardEvaluationContext context = new StandardEvaluationContext();
-        context.setVariables(map);
-        SpelExpressionParser el = new SpelExpressionParser();
-        String value = el.parseExpression("他的名字为#{#name}", new TemplateParserContext())
+        context.setVariables(Map.of("name","李林峰"));
+        String value = new SpelExpressionParser()
+                .parseExpression("他的名字为#{#name}", new TemplateParserContext())
                 .getValue(context,String.class);
         System.out.println(value);
     }
