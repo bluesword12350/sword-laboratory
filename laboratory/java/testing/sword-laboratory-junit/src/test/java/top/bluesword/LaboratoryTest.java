3e444d1e392882791566831dea25e5baf4c6dd37
李林峰 <yunlelongzai@qq.com>
2020-10-14 09:01:06 +0800

    junit5 assertThrows

--- laboratory/java/testing/sword-laboratory-junit/src/test/java/top/bluesword/LaboratoryTest.java
+++ laboratory/java/testing/sword-laboratory-junit/src/test/java/top/bluesword/LaboratoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LaboratoryTest {
@@ -12,6 +13,12 @@ public class LaboratoryTest {
     void print(){
         int anInt = new Random().nextInt(2);
         assertTrue(anInt<2);
+    }
+
+    @Test
+    void throwTest(){
+        String s = null;
+        assertThrows(NullPointerException.class,()-> System.out.println(s.length()));
     }
 
 }
