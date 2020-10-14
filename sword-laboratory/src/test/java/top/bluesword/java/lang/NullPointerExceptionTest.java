2d5832205ef6422789d4d82f4123fafe3e344192
李林峰 <yunlelongzai@qq.com>
2020-10-14 09:15:28 +0800

    java 14 增强型 NullPointerException 测试

---
+++ sword-laboratory/src/test/java/top/bluesword/java/lang/NullPointerExceptionTest.java
@@ -0,0 +1,20 @@
+package top.bluesword.java.lang;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class NullPointerExceptionTest {
+
+    /**
+     * java 14 增强型 NullPointerException
+     * 添加启动参数 `-XX:+ShowCodeDetailsInExceptionMessages`
+     */
+    @Test
+    void base(){
+        String s = null;
+        NullPointerException nullPointerException = assertThrows(NullPointerException.class, () -> System.out.println(s.length()));
+        nullPointerException.printStackTrace();
+    }
+
+}
