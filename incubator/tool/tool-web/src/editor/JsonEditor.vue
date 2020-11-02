0d99a3f7f2b5bf8cfaa8347bfc5eaccd2a5d4d01
李林峰 <yunlelongzai@qq.com>
2020-11-02 10:21:15 +0800

    代码格式整理

--- incubator/tool/tool-web/src/editor/JsonEditor.vue
+++ incubator/tool/tool-web/src/editor/JsonEditor.vue
@@ -1,27 +1,29 @@
 <template>
-    <div>
-        <div id="codeEditor" ref="codeEditor"></div>
-    </div>
+  <div>
+    <div id="codeEditor" ref="codeEditor"></div>
+  </div>
 </template>
 
 <script>
-    import JSONEditor from 'jsoneditor/dist/jsoneditor.min.js';
+import JSONEditor from 'jsoneditor/dist/jsoneditor.min.js';
 
-    export default {
-        name: "JsonEditor",
-        mounted() {
-            const container = this.$refs.codeEditor;
-            const editor = new JSONEditor(container, {
-                mode: 'tree',
-                modes: ['code', 'form', 'text', 'tree', 'view', 'preview']
-            })
+export default {
+  name: "JsonEditor",
+  mounted() {
+    new JSONEditor(
+        this.$refs.codeEditor,
+        {
+          mode: 'tree',
+          modes: ['code', 'form', 'text', 'tree', 'view', 'preview']
         }
-    }
+    )
+  }
+}
 </script>
 
 <style scoped>
-    #codeEditor {
-        width: 500px;
-        height: 500px;
-    }
+#codeEditor {
+  width: 500px;
+  height: 500px;
+}
 </style>
