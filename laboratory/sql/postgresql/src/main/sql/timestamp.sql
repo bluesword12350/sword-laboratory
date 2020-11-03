7ed9ec16bda9929dd3b258163efe3572eff9a9ff
李林峰 <yunlelongzai@qq.com>
2020-11-03 10:25:46 +0800

    postgre 日期函数测试

--- laboratory/sql/postgresql/src/main/sql/timestamp.sql
+++ laboratory/sql/postgresql/src/main/sql/timestamp.sql
@@ -1,17 +1,27 @@
 --https://www.postgresql.org/docs/10/functions-datetime.html#9.9.1
 
+--可选时区列表
+select * from pg_timezone_names;
+
 --日期转 unix 纪元 数字时间戳
 select extract(epoch from now());
 
 --时间戳格式化为字符串
-select to_char(now(),'YYYY-MM-DD HH24:MI:SS TZ');
-select to_char(now(),'YYYY-MM-DD HH24:MI:SS OF');
+select to_char(now(),'YYYY-MM-DD HH24:MI:SS.MS TZ');
+select to_char(now(),'YYYY-MM-DD HH24:MI:SS.MS OF');
+
+--字符串转时间戳
+select TIMESTAMP WITH TIME ZONE '2020-11-03 10:08:21.890+08';
+select to_char(TIMESTAMP '2020-11-03 10:08:21.890','YYYY-MM-DD HH24:MI:SS.MS OF');
 
 --当前日期
 select current_date;
+select current_time;
+SELECT current_timestamp,localtimestamp;
 
 --日期运算
 select current_date - interval '6 month';
 
 --日期比较
-select date '2020-04-07' >= current_date - interval '6 month'
+select date '2020-04-07' >= current_date - interval '6 month';
+
