28055b452c80d717ff6d51f497f7db4e6850d67a
李林峰 <yunlelongzai@qq.com>
2020-11-20 09:51:07 +0800

    MavenCrawler 任务添加

--- incubator/maven-search/src/main/java/top/bluesword/maven/search/MavenSearchApplication.java
+++ incubator/maven-search/src/main/java/top/bluesword/maven/search/MavenSearchApplication.java
@@ -4,6 +4,7 @@ package top.bluesword.maven.search;
  * 1.获取maven仓库软件包信息，构建依赖关系网 todo
  * 2.根据依赖关系网提供搜索功能 todo
  * 3.查看jar包文档 todo
+ * 4.搜索功能增强，根据被依赖次数排序 todo
  * @author 李林峰
  */
 public class MavenSearchApplication {
