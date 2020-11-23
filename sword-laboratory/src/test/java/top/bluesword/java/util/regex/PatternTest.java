d421e7055116dfe51b16c1612762f834e0b0d36e
李林峰 <yunlelongzai@qq.com>
2020-11-23 17:34:04 +0800

    Pattern 测试

--- sword-laboratory/src/test/java/top/bluesword/java/util/regex/PatternTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/util/regex/PatternTest.java
@@ -2,6 +2,7 @@ package top.bluesword.java.util.regex;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,5 +13,19 @@ class PatternTest {
         Pattern p = Pattern.compile("[0-9]+");
         Matcher m = p.matcher("1123");
         System.out.println(m.matches());
+    }
+
+    @Test
+    void find() {
+        Pattern p = Pattern.compile("[0-9]+");
+        Matcher m = p.matcher("a1123");
+        System.out.println(m.find());
+    }
+
+    @Test
+    void end() {
+        Pattern p = Pattern.compile("[0-9]+");
+        Matcher m = p.matcher("1123a4654");
+        m.results().map(MatchResult::group).forEach(System.out::println);
     }
 }
