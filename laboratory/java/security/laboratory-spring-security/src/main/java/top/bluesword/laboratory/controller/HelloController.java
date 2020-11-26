5684c561fb738ec8827ccf89d669990118da09e2
李林峰 <yunlelongzai@qq.com>
2020-11-26 16:57:08 +0800

    Spring Security 初步使用

---
+++ laboratory/java/security/laboratory-spring-security/src/main/java/top/bluesword/laboratory/controller/HelloController.java
@@ -0,0 +1,17 @@
+package top.bluesword.laboratory.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 李林峰
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
---
+++ laboratory/java/security/laboratory-spring-security/src/test/java/top/bluesword/laboratory/LaboratoryApplicationTests.java
@@ -0,0 +1,13 @@
+package top.bluesword.laboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
