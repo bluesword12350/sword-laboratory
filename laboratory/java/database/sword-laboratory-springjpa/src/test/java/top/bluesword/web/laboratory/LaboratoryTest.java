fdd310363aafbae55019997faa1ec655e3c914db
李林峰 <yunlelongzai@qq.com>
2020-12-07 14:34:25 +0800

    jpa spring boot 升级

---
+++ laboratory/java/database/sword-laboratory-springjpa/src/test/java/top/bluesword/web/laboratory/LaboratoryTest.java
@@ -0,0 +1,11 @@
+package top.bluesword.web.laboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LaboratoryTest {
+	
+	@Test
+	public void contextLoads() {}
+}
