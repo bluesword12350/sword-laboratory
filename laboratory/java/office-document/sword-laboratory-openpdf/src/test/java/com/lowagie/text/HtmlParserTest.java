8342f7dc18433cd23b2638b5b24093e9e26a7289
李林峰 <yunlelongzai@qq.com>
2020-12-14 13:58:06 +0800

    openpdf html转pdf

--- laboratory/java/office-document/sword-laboratory-openpdf/src/test/java/com/lowagie/text/HtmlParserTest.java
+++ laboratory/java/office-document/sword-laboratory-openpdf/src/test/java/com/lowagie/text/HtmlParserTest.java
@@ -1,19 +1,20 @@
 package com.lowagie.text;
 
+import com.lowagie.text.html.HtmlParser;
 import com.lowagie.text.pdf.PdfWriter;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class OpenpdfTest {
+public class HtmlParserTest {
 
     @Test
     void main() {
         try(Document document = new Document()) {
-            PdfWriter.getInstance(document, new FileOutputStream("HelloWorld.pdf"));
+            PdfWriter.getInstance(document, new FileOutputStream("ParseHtmlHelloWorld.pdf"));
             document.open();
-            document.add(new Paragraph("Hello World"));
+            HtmlParser.parse(document, this.getClass().getClassLoader().getResourceAsStream("test.html"));
         } catch (DocumentException | IOException de) {
             System.err.println(de.getMessage());
         }
