1c6ab5662ba0c40b7f923534f7ff39beb413ed32
李林峰 <yunlelongzai@qq.com>
2020-12-14 13:59:22 +0800

    freemarker 输出html文件

--- laboratory/java/util/laboratory-freemarker/src/test/java/freemarker/template/TemplateTest.java
+++ laboratory/java/util/laboratory-freemarker/src/test/java/freemarker/template/TemplateTest.java
@@ -9,12 +9,22 @@ public class TemplateTest {
 
     @Test
     void process() throws IOException, TemplateException {
+        StringWriter out = new StringWriter();
+        process(out);
+        System.out.println(out.toString());
+    }
+
+    @Test
+    void outFile() throws IOException, TemplateException {
+        process(new FileWriter("test.html"));
+    }
+
+    private void process(Writer out) throws IOException, TemplateException {
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_30);
         cfg.setDirectoryForTemplateLoading(new File("src/test/resources"));
         Template temp = cfg.getTemplate("test.ftlh");
         Map<String, String> root = Map.of("name", "Apache FreeMarker™");
-        StringWriter out = new StringWriter();
         temp.process(root, out);
-        System.out.println(out.toString());
     }
+
 }
