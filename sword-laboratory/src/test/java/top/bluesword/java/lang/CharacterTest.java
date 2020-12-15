8da2b3614bd796e5d5ffe66ff179e099e2dbbef9
李林峰 <yunlelongzai@qq.com>
2020-12-15 11:41:45 +0800

    Character 测试

--- sword-laboratory/src/test/java/top/bluesword/java/lang/CharacterTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/lang/CharacterTest.java
@@ -5,12 +5,8 @@ import org.junit.jupiter.api.Test;
 class CharacterTest {
     @Test
     void charTest(){
-        System.out.println("0=='"+(int)'0'+"'");
-        System.out.println("1=='"+(int)'1'+"'");
-        System.out.println("9=='"+(int)'9'+"'");
-        System.out.println("A=='"+(int)'A'+"'");
-        System.out.println("Z=='"+(int)'Z'+"'");
-        System.out.println("a=='"+(int)'a'+"'");
-        System.out.println("z=='"+(int)'z'+"'");
+        for (char i = 33; i <= 126; i++) {
+            System.out.println(i);
+        }
     }
 }
