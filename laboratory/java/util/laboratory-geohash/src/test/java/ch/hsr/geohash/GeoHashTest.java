25d04f6b567ec2cd51e4c51ed5634327da6bb822
李林峰 <yunlelongzai@qq.com>
2020-12-23 14:00:57 +0800

    geohash 测试

---
+++ laboratory/java/util/laboratory-geohash/src/test/java/ch/hsr/geohash/GeoHashTest.java
@@ -0,0 +1,12 @@
+package ch.hsr.geohash;
+
+import org.junit.jupiter.api.Test;
+
+public class GeoHashTest {
+
+    @Test
+    void withCharacterPrecision() {
+        GeoHash geoHash = GeoHash.withCharacterPrecision(39.9125, 116.40685612, 12);
+        System.out.println(geoHash);
+    }
+}
