a3543106dfc2dccfb79ac84f6233c81a9f7583ee
李林峰 <yunlelongzai@qq.com>
2021-01-26 17:50:58 +0800

    list sort 测试

--- sword-laboratory/src/test/java/top/bluesword/java/util/ListTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/util/ListTest.java
@@ -3,9 +3,11 @@ package top.bluesword.java.util;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class ListTest {
+
     @Test
     void ofTest(){
         List<Integer> list = List.of(1);
@@ -20,5 +22,15 @@ class ListTest {
         list2.add(23);
         System.out.println(list1.getClass());
         System.out.println(list2.getClass());
+    }
+
+    @Test
+    void sort() {
+        List<String> list1 = new ArrayList<>();
+        list1.add("23");
+        list1.add("231");
+        Comparator<String> comparing = Comparator.comparing(x -> (long) x.length());
+        list1.sort(comparing.reversed());
+        System.out.println(list1);
     }
 }
