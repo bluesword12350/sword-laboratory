536dfef0714b1d6852e62d43650415a29999e24a
李林峰 <yunlelongzai@qq.com>
2021-03-04 11:52:56 +0800

    NumberFormat 解析百分数测试

--- sword-laboratory/src/test/java/top/bluesword/java/text/NumberFormatTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/text/NumberFormatTest.java
@@ -3,11 +3,17 @@ package top.bluesword.java.text;
 import org.junit.jupiter.api.Test;
 
 import java.text.NumberFormat;
+import java.text.ParseException;
 
 public class NumberFormatTest {
 
     @Test
     void currencyFormat(){
         System.out.println(NumberFormat.getCurrencyInstance().format(12));
+    }
+
+    @Test
+    void parse() throws ParseException {
+        System.out.println(NumberFormat.getPercentInstance().parse("3.50%"));
     }
 }
