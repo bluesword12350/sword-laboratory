c8d47224c6ac7c3986653329ed35304144c58fa9
李林峰 <yunlelongzai@qq.com>
2021-03-10 16:58:38 +0800

    添加时间格式化文字

--- incubator/tool/tool-web/src/editor/Timestamp.vue
+++ incubator/tool/tool-web/src/editor/Timestamp.vue
@@ -1,35 +1,43 @@
 <template>
   <div style="padding:20px">
-    <a-row type="flex" :gutter="[horizontal,vertical]">
+    <a-row style="display: flex;align-items: center" :gutter="[horizontal,vertical]">
       <a-col style="width: 75px"><span>现在：</span></a-col>
-      <a-col style="padding-left: 15px"><a @click="copyTimestamp">{{nowTimestamp}}</a></a-col>
+      <a-col style="width: 150px;"><a @click="copyTimestamp">{{nowTimestamp}}</a></a-col>
+      <a-col style="width: 200px"><span>{{ nowDataTimeText }}</span></a-col>
     </a-row>
-    <a-row type="flex" :gutter="[horizontal,vertical]">
+    <a-row style="display: flex;align-items: center" :gutter="[horizontal,vertical]">
       <a-col style="width: 75px"><span>时间戳：</span></a-col>
-      <a-col><a-input ref="timestampDom"/></a-col>
+      <a-col style="width: 150px"><span ref="timestampDom"/></a-col>
+      <a-col style="width: 200px"><span>{{ dataTimeText }}</span></a-col>
     </a-row>
   </div>
 </template>
 
 <script>
+import dayjs from "dayjs"
+
 export default {
   name: "Timestamp",
   data() {
     return {
       nowTimestamp: 0,
+      nowDataTimeText: "",
+      dataTimeText:"",
       horizontal : 8,
       vertical : 16,
     }
   },
   mounted() {
-    setInterval(this.setNow,1);
+    setInterval(this.setNow,0);
   },
   methods:{
     setNow(){
       this.nowTimestamp = Date.now();
+      this.nowDataTimeText = dayjs(this.nowTimestamp).format();
     },
     copyTimestamp() {
-      this.$refs.timestampDom.setValue(this.nowTimestamp)
+      this.$refs.timestampDom.innerHTML= this.nowTimestamp
+      this.dataTimeText = this.nowDataTimeText;
     }
   },
 }
