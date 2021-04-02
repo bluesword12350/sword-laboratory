ee621d73c5c9912218557d99d98800cddaafbed4
李林峰 <yunlelongzai@qq.com>
2021-04-02 14:24:34 +0800

    H2 r2dbc 文件模式使用测试

---
+++ laboratory/java/database/service/laboratory-h2/src/test/java/top/bluesword/laboratory/DatabaseClientTest.java
@@ -0,0 +1,18 @@
+package top.bluesword.laboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.r2dbc.core.DatabaseClient;
+
+@SpringBootTest
+public class DatabaseClientTest {
+
+    @Autowired
+    DatabaseClient databaseClient;
+
+    @Test
+    void test() {
+        System.out.println(databaseClient.sql("select 1").fetch().all().blockFirst());
+    }
+}
