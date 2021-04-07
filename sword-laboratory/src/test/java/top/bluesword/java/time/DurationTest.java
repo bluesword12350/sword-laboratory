b11e9fd048124e44149a081ac8f2f66ca3d3de16
李林峰 <yunlelongzai@qq.com>
2021-04-07 18:59:01 +0800

    ChronoUnit between ZonedDateTime 测试； Duration 测试

---
+++ sword-laboratory/src/test/java/top/bluesword/java/time/DurationTest.java
@@ -0,0 +1,17 @@
+package top.bluesword.java.time;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+
+public class DurationTest {
+
+    @Test
+    void test() {
+        ZonedDateTime now = ZonedDateTime.now();
+        ZonedDateTime zonedDateTime = now.plusDays(-500);
+        Duration between = Duration.between(now, zonedDateTime);
+        System.out.println(between);
+    }
+}
--- sword-laboratory/src/test/java/top/bluesword/java/time/temporal/ChronoUnitTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/time/temporal/ChronoUnitTest.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.time.DateUtils;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
@@ -14,5 +15,12 @@ class ChronoUnitTest {
         Date date1 = DateUtils.parseDate("2019-04-08 08:00:00", "yyyy-MM-dd HH:mm:ss","yyyy-MM-dd");
         Date date2 = DateUtils.parseDate("2019-04-09 08:00:00", "yyyy-MM-dd HH:mm:ss","yyyy-MM-dd");
         System.out.println(ChronoUnit.HOURS.between(date2.toInstant(), date1.toInstant()));
+    }
+
+    @Test
+    void betweenZonedDateTime() {
+        ZonedDateTime now = ZonedDateTime.now();
+        ZonedDateTime zonedDateTime = now.plusDays(-500);
+        System.out.println(ChronoUnit.YEARS.between(zonedDateTime, now));
     }
 }
