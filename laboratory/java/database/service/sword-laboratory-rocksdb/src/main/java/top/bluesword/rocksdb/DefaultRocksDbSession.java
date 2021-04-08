670d57f27d6bce55cd1b30adf76f1b3176e10dbc
李林峰 <yunlelongzai@qq.com>
2021-04-08 11:30:00 +0800

    DefaultRocksDbSession 添加保存和查询

--- laboratory/java/database/service/sword-laboratory-rocksdb/src/main/java/top/bluesword/rocksdb/DefaultRocksDbSession.java
+++ laboratory/java/database/service/sword-laboratory-rocksdb/src/main/java/top/bluesword/rocksdb/DefaultRocksDbSession.java
@@ -1,12 +1,11 @@
 package top.bluesword.rocksdb;
 
-import org.rocksdb.ColumnFamilyHandle;
 import org.rocksdb.Options;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author 李林峰
@@ -25,13 +24,24 @@ public class DefaultRocksDbSession implements AutoCloseable {
 
     public static DefaultRocksDbSession init(String path) throws RocksDBException {
         final Options options = new Options();
-        List<ColumnFamilyHandle> columnFamilyHandleList = new ArrayList<>();
         RocksDB db;
         try(options) {
             options.setCreateIfMissing(true);
             db = RocksDB.open(options, path);
         }
         return new DefaultRocksDbSession(db);
+    }
+
+    public Optional<String> findString(String key) throws RocksDBException {
+        byte[] bytes = db.get(key.getBytes());
+        if (Objects.isNull(bytes)) {
+            return Optional.empty();
+        }
+        return Optional.of(new String(bytes));
+    }
+
+    public void saveString(String key, String value) throws RocksDBException {
+        db.put(key.getBytes(),value.getBytes());
     }
 
     @Override
