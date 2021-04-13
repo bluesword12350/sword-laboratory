3315d1953ae2b6b97e807a48898ceacf4de48d7d
李林峰 <yunlelongzai@qq.com>
2021-04-13 08:57:23 +0800

    ZonedDateTime format 测试

--- sword-laboratory/src/test/java/top/bluesword/java/time/ZonedDateTimeTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/time/ZonedDateTimeTest.java
@@ -3,12 +3,18 @@ package top.bluesword.java.time;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 class ZonedDateTimeTest {
 
     @Test
-    void test() {
+    void now() {
         ZonedDateTime now = ZonedDateTime.now();
         System.out.println(now.toString());
+    }
+
+    @Test
+    void format() {
+        System.out.println(ZonedDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZ")));
     }
 }
