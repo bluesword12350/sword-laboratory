75254e23fff433778eb07390d0e834f334925bcd
李林峰 <yunlelongzai@qq.com>
2021-04-28 15:11:38 +0800

    javafx 打包为可执行文件

--- laboratory/java/user-interface/sword-laboratory-javafx/src/main/shell/build-run.sh
+++ laboratory/java/user-interface/sword-laboratory-javafx/src/main/shell/build-run.sh
@@ -6,3 +6,6 @@ mvn clean javafx:jlink
 
 #镜像运行
 image/bin/java -m top.bluesword.laboratory.HelloFx
+
+#打包镜像为可执行文件
+jpackage --type app-image -n HelloFx -m HelloFx/top.bluesword.laboratory.HelloFx --runtime-image .
