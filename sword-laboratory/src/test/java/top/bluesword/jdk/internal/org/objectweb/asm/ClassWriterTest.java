abb654f8f9b9bcf34a7613b28a6ae41fdbe399e0
李林峰 <yunlelongzai@qq.com>
2021-05-10 14:46:30 +0800

    asm 方法调用

--- sword-laboratory/src/test/java/top/bluesword/jdk/internal/org/objectweb/asm/ClassWriterTest.java
+++ sword-laboratory/src/test/java/top/bluesword/jdk/internal/org/objectweb/asm/ClassWriterTest.java
@@ -30,8 +30,8 @@ class ClassWriterTest {
         {
             // 添加方法；修饰符、方法名、描述符、签名、异常
             MethodVisitor methodVisitor=classWriter.visitMethod(Opcodes.ACC_PUBLIC, "sum", "(II)I", null, null);
-            methodVisitor.visitVarInsn(Opcodes.ILOAD, 0);
             methodVisitor.visitVarInsn(Opcodes.ILOAD, 1);
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 2);
             methodVisitor.visitInsn(Opcodes.IADD);
             // 返回
             methodVisitor.visitInsn(Opcodes.IRETURN);
@@ -60,10 +60,27 @@ class ClassWriterTest {
             methodVisitor.visitLdcInsn("你好，ASM。");
             // 调用方法
             methodVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL,"java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+
+            // 创建对象
+            methodVisitor.visitTypeInsn(Opcodes.NEW, "AsmSumOfTwoNumbers");
+            methodVisitor.visitInsn(Opcodes.DUP);
+            methodVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL,"AsmSumOfTwoNumbers", "<init>", "()V", false);
+
+            methodVisitor.visitLdcInsn(15);
+            methodVisitor.visitLdcInsn(16);
+            methodVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL,"AsmSumOfTwoNumbers","sum","(II)I",false);
+            // 变量赋值
+            methodVisitor.visitVarInsn(Opcodes.ISTORE, 1);
+
+            methodVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System","out", "Ljava/io/PrintStream;");
+            // 加载变量
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 1);
+            methodVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL,"java/io/PrintStream", "println", "(I)V", false);
+
             // 返回
             methodVisitor.visitInsn(Opcodes.RETURN);
             // 设置操作数栈的深度和局部变量的大小
-            methodVisitor.visitMaxs(2, 1);
+            methodVisitor.visitMaxs(3, 2);
             // 方法结束
             methodVisitor.visitEnd();
         }
