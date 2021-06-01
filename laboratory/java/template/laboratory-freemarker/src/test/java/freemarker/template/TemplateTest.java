75a9901f1fce3f8cbfa47b41272161559154e9c5
李林峰 <yunlelongzai@qq.com>
2021-06-01 15:16:08 +0800

    模板分类建立

---
+++ laboratory/java/template/laboratory-freemarker/src/test/java/freemarker/template/TemplateTest.java
@@ -0,0 +1,30 @@
+package freemarker.template;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.Map;
+
+public class TemplateTest {
+
+    @Test
+    void process() throws IOException, TemplateException {
+        StringWriter out = new StringWriter();
+        process(out);
+        System.out.println(out);
+    }
+
+    @Test
+    void outFile() throws IOException, TemplateException {
+        process(new FileWriter("test.html"));
+    }
+
+    private void process(Writer out) throws IOException, TemplateException {
+        Configuration cfg = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
+        cfg.setDirectoryForTemplateLoading(new File("src/test/resources"));
+        Template temp = cfg.getTemplate("test.ftlh");
+        Map<String, String> root = Map.of("name", "Apache FreeMarker™");
+        temp.process(root, out);
+    }
+
+}
---
+++ laboratory/java/template/laboratory-spring-expression/src/test/java/org/springframework/expression/SpelExpressionParserTest.java
@@ -0,0 +1,21 @@
+package org.springframework.expression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.expression.common.TemplateParserContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import java.util.Map;
+
+class SpelExpressionParserTest {
+
+    @Test
+    void parseExpression(){
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        context.setVariables(Map.of("name","李林峰"));
+        String value = new SpelExpressionParser()
+                .parseExpression("他的名字为#{#name}", new TemplateParserContext())
+                .getValue(context,String.class);
+        System.out.println(value);
+    }
+}
