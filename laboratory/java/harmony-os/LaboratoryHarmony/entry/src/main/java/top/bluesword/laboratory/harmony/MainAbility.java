ca1de5fe663a68e1e1824e2ade22343f547cd4d9
李林峰 <yunlelongzai@qq.com>
2021-09-07 15:10:46 +0800

    鸿蒙项目模板

---
+++ laboratory/java/harmony-os/LaboratoryHarmony/entry/src/main/java/top/bluesword/laboratory/harmony/MainAbility.java
@@ -0,0 +1,13 @@
+package top.bluesword.laboratory.harmony;
+
+import top.bluesword.laboratory.harmony.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
