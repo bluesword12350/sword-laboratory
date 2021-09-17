7af2265de6a3d7640fd1dda2e3313f69d356fe3b
李林峰 <yunlelongzai@qq.com>
2021-09-17 18:37:02 +0800

    运行可执行jar包

--- laboratory/java/user-interface/sword-laboratory-javafx/src/main/shell/build-run.sh
+++ laboratory/java/user-interface/sword-laboratory-javafx/src/main/shell/build-run.sh
@@ -10,3 +10,6 @@ target/image/bin/java -m helloFx/top.bluesword.laboratory.HelloFx
 #打包镜像为可执行文件
 cd target
 jpackage --type app-image -n HelloFx -m helloFx/top.bluesword.laboratory.HelloFx --runtime-image image
+
+start /b javaw -jar *.jar
+exit
