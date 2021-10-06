4d20977eae8a51e2300ddcb0aafce4a289e311c7
李林峰 <yunlelongzai@qq.com>
2021-10-06 16:57:06 +0800

    vue3 使用测试

---
+++ laboratory/javascript/laboratory-vue/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <p>
+    <router-link to="/timestamp">Go to Bar</router-link>
+  </p>
+  <router-view></router-view>
+</template>
+
+<style>
+</style>
---
+++ laboratory/javascript/laboratory-vue/src/main.js
@@ -0,0 +1,15 @@
+import { createApp, h } from 'vue'
+import App from './App.vue'
+import * as VueRouter from 'vue-router'
+import Timestamp from "./components/Timestamp.vue";
+
+const routes = [
+    { path: '/timestamp', component: Timestamp }
+]
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+})
+
+createApp(App).use(router).mount('#app')
