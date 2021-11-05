710337dc6983bc0166b13ca8fde421968476df4e
李林峰 <yunlelongzai@qq.com>
2021-11-05 14:24:42 +0800

    ZoneId测试

--- sword-laboratory/src/test/java/top/bluesword/java/time/ZoneIdTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/time/ZoneIdTest.java
@@ -21,25 +21,34 @@ public class ZoneIdTest {
 
     @Test
     void ofOffset() {
-        ZoneId zoneId = ZoneId.ofOffset("GMT", ZoneOffset.ofTotalSeconds(28800));
-        System.out.println(zoneId);
+        for (String availableZoneId : ZoneId.getAvailableZoneIds()) {
+            ZoneOffset offset = ZoneId.of(availableZoneId).getRules().getOffset(Instant.now());
+            if (offset.getTotalSeconds() == 0) {
+                System.out.println("UTC+00:00");
+            } else {
+                System.out.println(ZoneId.ofOffset("UTC",offset).getId());
+            }
+        }
+    }
 
-        ZonedDateTime now = ZonedDateTime.now();
-        System.out.println(now);
-        System.out.println(now.toInstant().atZone(zoneId));
+    @Test
+    void normalized() {
+        for (String availableZoneId : ZoneId.getAvailableZoneIds()) {
+            System.out.println(ZoneId.of(availableZoneId).normalized());
+        }
     }
 
     @Test
     void of() {
-        String[] zoneIds = new String[]{"UTC+04:00","+09:30","GMT-5","Z","GMT+0"};
+        String[] zoneIds = new String[]{"UTC+04:00", "+09:30", "GMT-5", "Z", "GMT+0"};
         for (String zoneIdName : zoneIds) {
             ZoneId zoneId = ZoneId.of(zoneIdName);
-            System.out.println("zoneId                                     "+zoneId);
-            System.out.println("zoneId.getId()                             "+zoneId.getId());
+            System.out.println("zoneId                                     " + zoneId);
+            System.out.println("zoneId.getId()                             " + zoneId.getId());
             ZonedDateTime now = ZonedDateTime.now();
-            System.out.println("now                                        "+now);
-            System.out.println("now.toInstant().atZone(zoneId)             "+now.toInstant().atZone(zoneId));
-            System.out.println("zoneId.getRules().getOffset(Instant.now()) "+zoneId.getRules().getOffset(Instant.now()));
+            System.out.println("now                                        " + now);
+            System.out.println("now.toInstant().atZone(zoneId)             " + now.toInstant().atZone(zoneId));
+            System.out.println("zoneId.getRules().getOffset(Instant.now()) " + zoneId.getRules().getOffset(Instant.now()));
             System.out.println("-----------------");
         }
     }
