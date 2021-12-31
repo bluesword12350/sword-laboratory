04b87f898e108b191e8937d3844b5a2593ba83cc
李林峰 <yunlelongzai@qq.com>
2021-12-31 14:18:49 +0800

    Validator 小优化

--- laboratory/java/util/laboratory-hibernate-validator/src/main/java/top/bluesword/laboratory/controller/advice/ValidateControllerAdvice.java
+++ laboratory/java/util/laboratory-hibernate-validator/src/main/java/top/bluesword/laboratory/controller/advice/ValidateControllerAdvice.java
@@ -5,7 +5,6 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
