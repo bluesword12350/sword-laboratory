434e81c3984eb391ac80b3469c8ab2af7041bf3a
李林峰 <yunlelongzai@qq.com>
2022-01-04 14:24:27 +0800

    模态框组件再包装测试

---
+++ laboratory/javascript/laboratory-antdv/src/component/ModalComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-modal
+      v-model:visible="visible"
+      title="Title"
+      :confirm-loading="confirmLoading"
+      @ok="handleOk"
+  >
+    <p>{{ modalText }}</p>
+  </a-modal>
+</template>
+<script setup>
+import { ref} from 'vue';
+
+const modalText = ref('Content of the modal');
+const visible = ref(false);
+const confirmLoading = ref(false);
+
+const showModal = () => {
+  visible.value = true;
+};
+
+const handleOk = () => {
+  modalText.value = 'The modal will be closed after two seconds';
+  confirmLoading.value = true;
+  setTimeout(() => {
+    visible.value = false;
+    confirmLoading.value = false;
+  }, 2000);
+};
+
+expose({
+  showModal
+})
+</script>
--- laboratory/javascript/laboratory-antdv/src/page/ModalTest.vue
+++ laboratory/javascript/laboratory-antdv/src/page/ModalTest.vue
@@ -1,45 +1,16 @@
 <template>
   <div>
     <a-button type="primary" @click="showModal">Open Modal with async logic</a-button>
-    <a-modal
-        v-model:visible="visible"
-        title="Title"
-        :confirm-loading="confirmLoading"
-        @ok="handleOk"
-    >
-      <p>{{ modalText }}</p>
-    </a-modal>
+    <modal-component ref="modalComponent"/>
   </div>
 </template>
-<script>
-import { ref, defineComponent } from 'vue';
-export default defineComponent({
-  setup() {
-    const modalText = ref('Content of the modal');
-    const visible = ref(false);
-    const confirmLoading = ref(false);
+<script setup>
+import ModalComponent from "../component/ModalComponent.vue"
+import {ref} from "vue"
 
-    const showModal = () => {
-      visible.value = true;
-    };
+const modalComponent = ref(null)
 
-    const handleOk = () => {
-      modalText.value = 'The modal will be closed after two seconds';
-      confirmLoading.value = true;
-      setTimeout(() => {
-        visible.value = false;
-        confirmLoading.value = false;
-      }, 2000);
-    };
-
-    return {
-      modalText,
-      visible,
-      confirmLoading,
-      showModal,
-      handleOk,
-    };
-  },
-
-});
+let showModal = function () {
+  modalComponent.value.showModal()
+}
 </script>
