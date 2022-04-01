8f49aa72a057cc483e9dda4595ddfc1cfbff3f8f
李林峰 <yunlelongzai@qq.com>
2022-04-01 10:46:02 +0800

    Arrays 测试类调整

--- sword-laboratory/src/test/java/top/bluesword/java/util/ArraysTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/util/ArraysTest.java
@@ -1,22 +1,28 @@
 package top.bluesword.java.util;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ArraysTest {
-	public static void main(String[] args) {
+class ArraysTest {
+
+	@Test
+	void asList() {
 		String[] strings = {"1","2"};
 		List<String> asList = new ArrayList<>(Arrays.asList(strings));
 		asList.remove(0);
 		System.out.println(asList);
 	}
-	
-	static void copyOfTest() {
+
+	@Test
+	void copyOfTest() {
 		String[] strings = {"1","2"};
 		String[] copyOf = Arrays.copyOf(strings, 1);
 		String[] copyOfRange = Arrays.copyOfRange(strings, 1, strings.length);
 		System.out.println(Arrays.toString(copyOf));
 		System.out.println(Arrays.toString(copyOfRange));
 	}
+
 }
