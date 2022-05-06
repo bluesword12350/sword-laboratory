bf803d43c5b99839641d5fafe68c84e5329fa10c
李林峰 <yunlelongzai@qq.com>
2022-05-06 15:45:22 +0800

    spring jdbc NamedParameterJdbcTemplate 测试

--- laboratory/java/database/client/sword-laboratory-jdbc/src/test/java/top/bluesword/laboratory/JdbcTemplateTest.java
+++ laboratory/java/database/client/sword-laboratory-jdbc/src/test/java/top/bluesword/laboratory/JdbcTemplateTest.java
@@ -1,4 +1,4 @@
-package top.bluesword.web.laboratory;
+package top.bluesword.laboratory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,14 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
-public class LaboratoryTest {
+public class JdbcTemplateTest {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	
 	@Test
 	public void contextLoads() {
-		String sql = "SELECT 1 where 1 = ? and 'a' in (?)";
-		String[] strings = {"a"};
-		System.out.println(jdbcTemplate.queryForList(sql,1,strings));
+		String sql = "SELECT 1 where 1 = ?";
+		System.out.println(jdbcTemplate.queryForList(sql,1));
 	}
 }
