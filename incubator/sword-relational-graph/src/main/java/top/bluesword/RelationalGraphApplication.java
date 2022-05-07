9e7e58c63b0e60064c133b841c8897ac05c64e95
李林峰 <yunlelongzai@qq.com>
2022-05-07 16:37:08 +0800

    关系图绘制项目初始化

--- incubator/sword-relational-graph/src/main/java/top/bluesword/RelationalGraphApplication.java
+++ incubator/sword-relational-graph/src/main/java/top/bluesword/RelationalGraphApplication.java
@@ -1,10 +1,7 @@
 package top.bluesword;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author 李林峰
@@ -12,11 +9,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class RelationalGraphApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(RelationalGraphApplication.class);
-
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(RelationalGraphApplication.class, args);
-		log.info("bean总数:{}",ctx.getBeanDefinitionCount());
+		SpringApplication.run(RelationalGraphApplication.class, args);
 	}
 
 }
