bf910e961a50464a988a39cc40ebae1abbf4007e
李林峰 <yunlelongzai@qq.com>
2022-05-14 17:48:10 +0800

    优化注解测试类

--- sword-laboratory/src/test/java/top/bluesword/java/lang/annotation/FruitRunTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/lang/annotation/FruitRunTest.java
@@ -1,10 +1,14 @@
 package top.bluesword.java.lang.annotation;
 
+import org.junit.jupiter.api.Test;
 import top.bluesword.java.lang.annotation.bean.Apple;
 import top.bluesword.java.lang.annotation.util.FruitInfoUtil;
 
-public class FruitRun {
-    public static void main(String[] args) {
+class FruitRunTest {
+
+    @Test
+    void main() {
         FruitInfoUtil.getFruitInfo(Apple.class);
     }
+
 }
--- sword-laboratory/src/test/java/top/bluesword/java/lang/annotation/util/FruitInfoUtil.java
+++ sword-laboratory/src/test/java/top/bluesword/java/lang/annotation/util/FruitInfoUtil.java
@@ -1,10 +1,10 @@
 package top.bluesword.java.lang.annotation.util;
 
-import java.lang.reflect.Field;
-
 import top.bluesword.java.lang.annotation.FruitColor;
 import top.bluesword.java.lang.annotation.FruitName;
 import top.bluesword.java.lang.annotation.FruitProvider;
+
+import java.lang.reflect.Field;
 
 /**
  * 注解处理器
@@ -14,7 +14,7 @@ public class FruitInfoUtil {
         
         String strFruitName=" 水果名称：";
         String strFruitColor=" 水果颜色：";
-        String strFruitProvicer="供应商信息：";
+        String strFruitProvider;
         
         Field[] fields = clazz.getDeclaredFields();
         
@@ -25,14 +25,14 @@ public class FruitInfoUtil {
                 System.out.println(strFruitName);
             }
             else if(field.isAnnotationPresent(FruitColor.class)){
-                FruitColor fruitColor= (FruitColor) field.getAnnotation(FruitColor.class);
+                FruitColor fruitColor= field.getAnnotation(FruitColor.class);
                 strFruitColor=strFruitColor+fruitColor.fruitColor().toString();
                 System.out.println(strFruitColor);
             }
             else if(field.isAnnotationPresent(FruitProvider.class)){
-                FruitProvider fruitProvider= (FruitProvider) field.getAnnotation(FruitProvider.class);
-                strFruitProvicer=" 供应商编号："+fruitProvider.id()+" 供应商名称："+fruitProvider.name()+" 供应商地址："+fruitProvider.address();
-                System.out.println(strFruitProvicer);
+                FruitProvider fruitProvider= field.getAnnotation(FruitProvider.class);
+                strFruitProvider=" 供应商编号："+fruitProvider.id()+" 供应商名称："+fruitProvider.name()+" 供应商地址："+fruitProvider.address();
+                System.out.println(strFruitProvider);
             }
         }
     }
