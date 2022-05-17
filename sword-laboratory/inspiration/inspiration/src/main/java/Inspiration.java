39094c916425f97ecb1b062acc274f3468278d52
李林峰 <yunlelongzai@qq.com>
2022-05-17 13:49:21 +0800

    圆周率

--- sword-laboratory/inspiration/inspiration/src/main/java/Inspiration.java
+++ sword-laboratory/inspiration/inspiration/src/main/java/Inspiration.java
@@ -1,15 +1,12 @@
-import com.google.gson.Gson;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
+/**
+ * @author 李林峰
+ */
 public class Inspiration {
     public static void main(String[] args) throws URISyntaxException, IOException {
         URL piUrl = ClassLoader.getSystemResource("PI.txt");
@@ -19,14 +16,6 @@ public class Inspiration {
         try(piInput) {
             pi = new String(piInput.readAllBytes());
         }
-        List<Map<String,Integer>> data = new ArrayList<>();
-        for (int i = 0; i < 366; i++) {
-            int num = Integer.parseInt(String.valueOf(pi.charAt(i)));
-            Map<String,Integer> map = new HashMap<>();
-            map.put("day",i+1);
-            map.put("num",num);
-            data.add(map);
-        }
-        System.out.println(new Gson().toJson(data));
+        System.out.println(pi);
     }
 }
