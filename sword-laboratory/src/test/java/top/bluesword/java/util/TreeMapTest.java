e5e4a130dfbfa1d3c69b933b3c33a1c43ef028a1
李林峰 <yunlelongzai@qq.com>
2022-05-24 20:32:03 +0800

    TreeMap 测试优化

--- sword-laboratory/src/test/java/top/bluesword/java/util/TreeMapTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/util/TreeMapTest.java
@@ -2,16 +2,14 @@ package top.bluesword.java.util;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeMap;
 
 class TreeMapTest {
-	
-	private static TreeMap<Integer, Integer> treeMap = new TreeMap<>(Comparator.comparingInt(o -> o));
 
 	@Test
 	void put() {
+		TreeMap<Integer, Integer> treeMap = new TreeMap<>(Integer::compareTo);
 		treeMap.put(9, 1);
 		treeMap.put(2, 1);
 		treeMap.put(5, 1);
