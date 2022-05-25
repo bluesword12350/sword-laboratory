390c2694cfc01def244742b43904b9affbd5934c
李林峰 <yunlelongzai@qq.com>
2022-05-25 15:11:44 +0800

    jackson xml 基本使用测试

---
+++ laboratory/java/data-interchange/laboratory-jackson-xml/src/test/java/javax/xml/bind/XmlMapperTest.java
@@ -0,0 +1,19 @@
+package javax.xml.bind;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
+import org.junit.jupiter.api.Test;
+import tob.bluesword.Metadata;
+
+public class XmlMapperTest {
+
+    @Test
+    void unmarshal() throws JsonProcessingException {
+        Metadata metadata = new Metadata();
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.enable(ToXmlGenerator.Feature.WRITE_XML_DECLARATION);
+        System.out.println(xmlMapper.writeValueAsString(metadata));
+    }
+
+}
---
+++ laboratory/java/data-interchange/laboratory-jackson-xml/src/test/java/tob/bluesword/Metadata.java
@@ -0,0 +1,34 @@
+package tob.bluesword;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Metadata {
+
+    private String groupId;
+
+    private String artifactId;
+
+    private Versioning versioning;
+
+    @Data
+    static class Versioning {
+
+        private String latest;
+
+        private String release;
+
+        private Versions versions;
+
+        private Long lastUpdated;
+
+        @Data
+        static class Versions {
+
+            private List<String> version;
+
+        }
+    }
+}
