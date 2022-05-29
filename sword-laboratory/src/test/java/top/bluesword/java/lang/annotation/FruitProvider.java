7b9e0b78fd7d88d6906a2c91ebabe68c5c732082
李林峰 <yunlelongzai@qq.com>
2022-05-29 19:44:24 +0800

    代码优化

--- sword-laboratory/src/test/java/top/bluesword/java/lang/annotation/FruitProvider.java
+++ sword-laboratory/src/test/java/top/bluesword/java/lang/annotation/FruitProvider.java
@@ -3,6 +3,7 @@ package top.bluesword.java.lang.annotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
@@ -17,15 +18,16 @@ public @interface FruitProvider {
     /**
      * 供应商编号
      */
-    public int id() default -1;
+    int id() default -1;
     
     /**
      * 供应商名称
      */
-    public String name() default "";
+    String name() default "";
     
     /**
      * 供应商地址
      */
-    public String address() default "";
+    String address() default "";
+
 }
