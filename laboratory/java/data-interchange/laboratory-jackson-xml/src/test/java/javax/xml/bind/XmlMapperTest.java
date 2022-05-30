392f67fdfa32d30d6343896d641244c22fb88a4e
李林峰 <yunlelongzai@qq.com>
2022-05-30 08:30:29 +0800

    jackson xml 解析xml 文档相对于模型多出字段不报错测试

--- laboratory/java/data-interchange/laboratory-jackson-xml/src/test/java/javax/xml/bind/XmlMapperTest.java
+++ laboratory/java/data-interchange/laboratory-jackson-xml/src/test/java/javax/xml/bind/XmlMapperTest.java
@@ -1,6 +1,7 @@
 package javax.xml.bind;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,14 @@ public class XmlMapperTest {
         XmlMapper xmlMapper = new XmlMapper();
         xmlMapper.enable(ToXmlGenerator.Feature.WRITE_XML_DECLARATION);
         System.out.println(xmlMapper.writeValueAsString(metadata));
+    }
+
+    @Test
+    void readValue() throws JsonProcessingException {
+        final String xml = "<?xml version='1.0' encoding='UTF-8'?><Metadata><groupId/><artifactId/><versioning/><ref/></Metadata>";
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        System.out.println(xmlMapper.readValue(xml,Metadata.class));
     }
 
 }
