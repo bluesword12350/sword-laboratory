1a4d417beffcdaeddb039559f68e4a6abc3f3e3d
李林峰 <yunlelongzai@qq.com>
2022-06-06 14:16:56 +0800

    添加行点击事件

--- incubator/sword-relational-graph/src/main/web/App.vue
+++ incubator/sword-relational-graph/src/main/web/App.vue
@@ -1,14 +1,18 @@
 <template>
   <el-button type="primary">新建关系图</el-button>
-  <el-table :data="data" style="width: 100%">
+  <el-table :data="data" style="width: 100%" @row-click="rowClick">
     <el-table-column prop="fileName" label="文件名" align="center"/>
   </el-table>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 const data = [
   {
     "fileName":"文件1"
   }
 ]
+
+function rowClick(row) {
+  console.log("row",row)
+}
 </script>
