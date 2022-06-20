e45361a72e2fc1fd43888abc933ff41b5469b137
李林峰 <yunlelongzai@qq.com>
2022-06-20 17:19:07 +0800

    日志工具更换

--- sword-laboratory/src/test/java/top/bluesword/enums/CurrencyCodeEnumTest.java
+++ sword-laboratory/src/test/java/top/bluesword/enums/CurrencyCodeEnumTest.java
@@ -1,12 +1,16 @@
 package top.bluesword.enums;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class CurrencyCodeEnumTest {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Test
     void test(){
-        System.out.println(CurrencyCodeEnum.valueOf("CNY"));
+        logger.info(CurrencyCodeEnum.valueOf("CNY").toString());
     }
 
 }
