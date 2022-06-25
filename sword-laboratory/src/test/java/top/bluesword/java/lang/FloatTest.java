6ddf2f60a5a4c413a7236e08a636dfdc10343dba
李林峰 <yunlelongzai@qq.com>
2022-06-25 21:32:24 +0800

    优化Float测试

--- sword-laboratory/src/test/java/top/bluesword/java/lang/FloatTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/lang/FloatTest.java
@@ -1,6 +1,8 @@
 package top.bluesword.java.lang;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Random;
 
@@ -9,19 +11,21 @@ import java.util.Random;
  */
 class FloatTest {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Test
     void toBinaryString(){
         float value = new Random().nextFloat();
         String binaryString = Long.toBinaryString(Float.floatToIntBits(value));
         binaryString = String.format("%0"+(32-binaryString.length())+"d%s",0,binaryString);
-        System.out.println(value);
-        System.out.println(binaryString);
-        System.out.println("============sign============");
-        System.out.println(binaryString.substring(0, 1));
-        System.out.println("============biased exponent============");
-        System.out.println(binaryString.substring(1,9));
-        System.out.println("============fraction============");
-        System.out.println(binaryString.substring(9));
+        log.info("{}",value);
+        log.info(binaryString);
+        log.info("============sign============");
+        log.info("{}",binaryString.charAt(0));
+        log.info("============biased exponent============");
+        log.info(binaryString.substring(1, 9));
+        log.info("============fraction============");
+        log.info(binaryString.substring(9));
     }
 
 }
