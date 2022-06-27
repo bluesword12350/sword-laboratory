7ba144098e6ed560697730bd8d6c89716e52764a
李林峰 <yunlelongzai@qq.com>
2022-06-27 17:16:52 +0800

    Stream 测试优化

--- sword-laboratory/src/test/java/top/bluesword/java/util/stream/StreamTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/util/stream/StreamTest.java
@@ -1,30 +1,28 @@
 package top.bluesword.java.util.stream;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 class StreamTest {
 
-    @Test
-    void of(){
-        System.out.println(Stream.of("1", "2", "3"));
-    }
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Test
-    void stream(){
-        System.out.println(List.of("1", "2", "3").stream());
+    void of(){
+        log.info("{}",Stream.of("1", "2", "3"));
     }
 
     @Test
     void forEach(){
-        Stream.of("1", "2", "3").forEach(System.out::print);
+        Stream.of("1", "2", "3").forEach(log::info);
     }
 
     @Test
     void filter(){
-        Stream.of("1", "2", "3").filter("1"::equals).forEach(System.out::print);
+        Stream.of("1", "2", "3").filter("1"::equals).forEach(log::info);
     }
 
 }
