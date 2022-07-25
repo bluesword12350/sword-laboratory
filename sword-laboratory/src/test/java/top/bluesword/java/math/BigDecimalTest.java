e3421dc5f0126c9834fbb6d3fb5f9704005eb871
李林峰 <yunlelongzai@qq.com>
2022-07-25 15:27:25 +0800

    BigDecimal 科学计数法测试

--- sword-laboratory/src/test/java/top/bluesword/java/math/BigDecimalTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/math/BigDecimalTest.java
@@ -1,58 +1,71 @@
 package top.bluesword.java.math;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Slf4j
 class BigDecimalTest {
 
 	@Test
 	void setScale() {
 		BigDecimal decimal = new BigDecimal("100000.215");
 		BigDecimal newDecimal = decimal.setScale(2,RoundingMode.HALF_UP);
-		System.out.println(newDecimal);
+		log.info("{}",newDecimal);
 	}
 
 	@Test
 	void divide() {
-		System.out.println(BigDecimal.ONE.divide(BigDecimal.valueOf(3),5,RoundingMode.HALF_UP));
+		log.info("{}",BigDecimal.ONE.divide(BigDecimal.valueOf(3),5,RoundingMode.HALF_UP));
 	}
 
 	@Test
 	void intValue() {
 		int i = new BigDecimal("1.99").intValue();
-		System.out.println(i);
+		log.info("{}",i);
 	}
 
 	@Test
 	void equals() {
-		System.out.println(BigDecimal.ZERO.equals(BigDecimal.valueOf(0.0000)));
+		log.info("{}",BigDecimal.ZERO.equals(BigDecimal.valueOf(0.0000)));
 	}
 
 	@Test
 	void zeroEquals() {
-		System.out.println(0 == new BigDecimal("0.00000").doubleValue());
-		System.out.println(BigDecimal.ZERO.equals(BigDecimal.valueOf(0.0000)));
-		System.out.println(BigDecimal.ZERO.compareTo(BigDecimal.valueOf(0.0000))==0);
+		log.info("{}",0 == new BigDecimal("0.00000").doubleValue());
+		log.info("{}",BigDecimal.ZERO.equals(BigDecimal.valueOf(0.0000)));
+		log.info("{}",BigDecimal.ZERO.compareTo(BigDecimal.valueOf(0.0000))==0);
 	}
 
     @Test
     void compareTo() {
-        System.out.println(BigDecimal.ZERO.compareTo(BigDecimal.valueOf(0.0000)));
-        System.out.println(BigDecimal.valueOf(0.0000).compareTo(BigDecimal.ZERO));
-        System.out.println(BigDecimal.ZERO.compareTo(BigDecimal.valueOf(0.0001)));
-        System.out.println(BigDecimal.valueOf(0.0001).compareTo(BigDecimal.ZERO));
+        log.info("{}",BigDecimal.ZERO.compareTo(BigDecimal.valueOf(0.0000)));
+        log.info("{}",BigDecimal.valueOf(0.0000).compareTo(BigDecimal.ZERO));
+        log.info("{}",BigDecimal.ZERO.compareTo(BigDecimal.valueOf(0.0001)));
+        log.info("{}",BigDecimal.valueOf(0.0001).compareTo(BigDecimal.ZERO));
     }
 
 	@Test
 	void multiply() {
 		BigDecimal decimal = BigDecimal.ONE.divide(BigDecimal.valueOf(3), 5, RoundingMode.HALF_UP);
-		System.out.println(decimal.multiply(decimal));
+		log.info("{}",decimal.multiply(decimal));
 	}
 
 	@Test
 	void string() {
-		System.out.println(new BigDecimal("0.000000"));
+		log.info("{}",new BigDecimal("0.000000"));
 	}
+
+	@Test
+	void scientificNotation() {
+		log.info("-10:{}",BigDecimal.valueOf(Math.pow(10, -10)).stripTrailingZeros().toPlainString());
+		log.info(" -7:{}",BigDecimal.valueOf(Math.pow(10, -7)));
+		log.info(" -6:{}",BigDecimal.valueOf(Math.pow(10, -6)));
+		log.info("  6:{}",BigDecimal.valueOf(Math.pow(10, 6)).stripTrailingZeros());
+		log.info("  7:{}",BigDecimal.valueOf(Math.pow(10, 7)));
+		log.info(" 10:{}",BigDecimal.valueOf(Math.pow(10, 10)).toPlainString());
+	}
+
 }
