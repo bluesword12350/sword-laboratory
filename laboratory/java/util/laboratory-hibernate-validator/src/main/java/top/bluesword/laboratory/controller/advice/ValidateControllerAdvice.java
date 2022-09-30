c8cb5b8d4802ec96d063ec6e14546db8247ab1a0
李林峰 <yunlelongzai@qq.com>
2022-09-30 11:14:00 +0800

    spring validator ValidateControllerAdvice 异常信息优化

--- laboratory/java/util/laboratory-hibernate-validator/src/main/java/top/bluesword/laboratory/controller/advice/ValidateControllerAdvice.java
+++ laboratory/java/util/laboratory-hibernate-validator/src/main/java/top/bluesword/laboratory/controller/advice/ValidateControllerAdvice.java
@@ -2,12 +2,12 @@ package top.bluesword.laboratory.controller.advice;
 
 import org.springframework.beans.NotReadablePropertyException;
 import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
+
+import java.util.List;
 
 /**
  * @author 李林峰
@@ -23,29 +23,18 @@ public class ValidateControllerAdvice {
      * @see org.springframework.validation.DataBinder
      */
     @ExceptionHandler(BindException.class)
-    public String validExceptionHandler(BindException e) {
-        return toMessage(e.getBindingResult());
-    }
-
-    private String toMessage(BindingResult bindingResult) {
-        StringBuilder build = new StringBuilder().append("[");
-        for (FieldError f : bindingResult.getFieldErrors()) {
-            build.append(f.getField()).append(':').append(f.getDefaultMessage()).append(';');
-        }
-        for (ObjectError g : bindingResult.getGlobalErrors()) {
-            build.append(g.getDefaultMessage()).append(';');
-        }
-        build.setCharAt(build.length()-1,']');
-        return build.toString();
+    public List<ObjectError> validExceptionHandler(BindException e) {
+        return e.getBindingResult().getAllErrors();
     }
 
     @ExceptionHandler(WebExchangeBindException.class)
-    public String exceptionHandler(WebExchangeBindException e) {
-        return toMessage(e.getBindingResult());
+    public List<ObjectError> exceptionHandler(WebExchangeBindException e) {
+        return e.getBindingResult().getAllErrors();
     }
 
     @ExceptionHandler(NotReadablePropertyException.class)
     public String exceptionHandler(NotReadablePropertyException e) {
         return e.getLocalizedMessage();
     }
+
 }
