50ec7397b02ef6dbc2c282001c3f84831fb64c68
李林峰 <yunlelongzai@qq.com>
2022-10-10 09:31:23 +0800

    mysql 实验项目迁移

--- laboratory/java/database/service/sword-laboratory-mysql/src/main/java/top/bluesword/web/laboratory/dao/UserRoleMapper.java
+++
@@ -1,7 +0,0 @@
-package top.bluesword.web.laboratory.dao;
-
-import tk.mybatis.mapper.common.Mapper;
-import top.bluesword.web.laboratory.entity.UserRole;
-
-public interface UserRoleMapper extends Mapper<UserRole> {
-}
--- laboratory/java/database/service/sword-laboratory-mysql/src/main/java/top/bluesword/web/laboratory/dao/provider/RoleMapperProvider.java
+++
@@ -1,9 +0,0 @@
-package top.bluesword.web.laboratory.dao.provider;
-
-public class RoleMapperProvider{
-	
-	public String getRoleListByUserid(Integer userId) {
-		System.out.println("SQL:getRoleListByUserid开始生成");
-        return "SELECT * FROM role LEFT JOIN user_role ur ON ur.role_id = role.id WHERE ur.user_id = "+userId;
-    }
-}
