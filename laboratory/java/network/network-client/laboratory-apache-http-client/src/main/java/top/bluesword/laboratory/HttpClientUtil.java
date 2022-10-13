e597dcccc85aebc0bf9209ce0ffc86dc10d8a3ef
李林峰 <yunlelongzai@qq.com>
2022-10-13 15:08:18 +0800

    apache httpclient 升级到 5.1.3

--- laboratory/java/network/network-client/laboratory-apache-http-client/src/main/java/top/bluesword/laboratory/HttpClientUtil.java
+++ laboratory/java/network/network-client/laboratory-apache-http-client/src/main/java/top/bluesword/laboratory/HttpClientUtil.java
@@ -1,28 +1,33 @@
 package top.bluesword.laboratory;
 
 import org.apache.commons.codec.CharEncoding;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.Header;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.util.EntityUtils;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.hc.core5.http.message.BasicHeader;
+import org.apache.hc.core5.http.message.BasicNameValuePair;
+import org.apache.hc.core5.net.URIBuilder;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -36,29 +41,26 @@ import java.util.Objects;
  */
 public class HttpClientUtil {
 
-    public static String sendUnsafeHttpsGet(String url, Map<String, String> paramMap, Map<String, String> headersMap) throws IOException {
+    public static String sendUnsafeHttpsGet(String url, Map<String, String> paramMap, Map<String, String> headersMap) throws IOException, ParseException, URISyntaxException {
         try (CloseableHttpClient httpClient = createUnsafeSslClient()) {
             return sendGetForString(url, paramMap, headersMap, httpClient);
         }
     }
 
-    public static String sendGetForString(String url, Map<String, String> paramMap, Map<String, String> headersMap) throws IOException {
+    public static String sendGetForString(String url, Map<String, String> paramMap, Map<String, String> headersMap) throws IOException, ParseException, URISyntaxException {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             return sendGetForString(url, paramMap, headersMap, httpClient);
         }
     }
 
-    private static String sendGetForString(String url, Map<String, String> paramMap, Map<String, String> headersMap, CloseableHttpClient httpClient) throws IOException {
+    private static String sendGetForString(String url, Map<String, String> paramMap, Map<String, String> headersMap, CloseableHttpClient httpClient) throws IOException, ParseException, URISyntaxException {
         CloseableHttpResponse response = sendGet(url, paramMap, headersMap, httpClient);
         return EntityUtils.toString(response.getEntity());
     }
 
-    private static CloseableHttpResponse sendGet(String url, Map<String, String> paramMap, Map<String, String> headersMap, CloseableHttpClient httpClient) throws IOException {
-        String param = URLEncodedUtils.format(toParamMap(paramMap), CharEncoding.UTF_8);
-        if (StringUtils.isNotBlank(param)) {
-            param = "?" + param;
-        }
-        HttpGet httpGet = new HttpGet(url + param);
+    private static CloseableHttpResponse sendGet(String url, Map<String, String> paramMap, Map<String, String> headersMap, CloseableHttpClient httpClient) throws IOException, URISyntaxException {
+        URI uri = new URIBuilder(url).setParameters(toParamMap(paramMap)).build();
+        HttpGet httpGet = new HttpGet(uri);
         if (headersMap != null) {
             for (Map.Entry<String, String> entry : headersMap.entrySet()) {
                 httpGet.setHeader(entry.getKey(), entry.getValue());
@@ -67,16 +69,16 @@ public class HttpClientUtil {
         return httpClient.execute(httpGet);
     }
 
-    private static InputStream sendGetForInputStream(String url, CloseableHttpClient httpClient) throws IOException {
+    private static InputStream sendGetForInputStream(String url, CloseableHttpClient httpClient) throws IOException, URISyntaxException {
         CloseableHttpResponse closeableHttpResponse = sendGet(url, null, null, httpClient);
         return closeableHttpResponse.getEntity().getContent();
     }
 
-    public static String sendPostJson(String url, String json,Map<String, String> headersMap) throws IOException {
+    public static String sendPostJson(String url, String json,Map<String, String> headersMap) throws IOException, ParseException {
         return sendPostJson(url,json,toHeaders(headersMap));
     }
 
-    public static String sendPostJson(String url, String json) throws IOException {
+    public static String sendPostJson(String url, String json) throws IOException, ParseException {
         return sendPostJson(url,json,new Header[0]);
     }
 
@@ -87,10 +89,10 @@ public class HttpClientUtil {
      * @param json 请求参数(json字符串)
      * @return 接收的参数
      */
-    public static String sendPostJson(String url, String json, Header[] headers) throws IOException {
+    public static String sendPostJson(String url, String json, Header[] headers) throws IOException, ParseException {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpPost httpPost = new HttpPost(url);
-            StringEntity entity = new StringEntity(json, CharEncoding.UTF_8);
+            StringEntity entity = new StringEntity(json, StandardCharsets.UTF_8);
             httpPost.setEntity(entity);
             httpPost.setHeader("Content-Encoding", CharEncoding.UTF_8);
             httpPost.setHeader("content-type", "application/json");
@@ -107,10 +109,10 @@ public class HttpClientUtil {
      * @param headers 请求头
      * @return 返回参数
      */
-    public static String sendPostParam(String url, Map<String, String> paramMap, Header[] headers) throws IOException {
+    public static String sendPostParam(String url, Map<String, String> paramMap, Header[] headers) throws IOException, ParseException {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpPost httpPost = new HttpPost(url);
-            httpPost.setEntity(new UrlEncodedFormEntity(toParamMap(paramMap), CharEncoding.UTF_8));
+            httpPost.setEntity(new UrlEncodedFormEntity(toParamMap(paramMap), StandardCharsets.UTF_8));
             httpPost.setHeaders(headers);
             CloseableHttpResponse response = httpClient.execute(httpPost);
             return EntityUtils.toString(response.getEntity());
@@ -143,7 +145,8 @@ public class HttpClientUtil {
             SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, (chain, authType) -> true).build();
             HostnameVerifier hostnameVerifier = NoopHostnameVerifier.INSTANCE;
             SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
-            return HttpClients.custom().setSSLSocketFactory(sslConnectionSocketFactory).build();
+            PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create().setSSLSocketFactory(sslConnectionSocketFactory).build();
+            return HttpClients.custom().setConnectionManager(connectionManager).build();
         } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
             return HttpClients.createDefault();
         }
