20b256ce0036e32ae27fa0ede06a561c2b160370
李林峰 <yunlelongzai@qq.com>
2022-11-03 13:51:05 +0800

    groovy JsonSlurper 调整文件

--- laboratory/groovy/laboratory-groovy/src/test/groovy/groovy/json/JsonSlurperTest.groovy
+++ laboratory/groovy/laboratory-groovy/src/test/groovy/groovy/json/JsonSlurperTest.groovy
@@ -6,7 +6,7 @@ class JsonSlurperTest {
 
     @Test
     void test(){
-        def file = new File("D:\\workspace\\bluesword\\sword-laboratory\\laboratory\\groovy\\laboratory-groovy\\data\\2022-11-02推送数据.json")
+        def file = new File("data/test.json")
         def jsonSlurper = new JsonSlurper()
         def object = jsonSlurper.parse(file)
         print object[0].customer_order_no
