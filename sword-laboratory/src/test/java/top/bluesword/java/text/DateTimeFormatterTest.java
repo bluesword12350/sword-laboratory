616511c4daf96e6d8ba9f746d93560ba333c6453
李林峰 <yunlelongzai@qq.com>
2022-12-09 16:28:14 +0800

    DateTimeFormatterBuilder 使用测试

--- sword-laboratory/src/test/java/top/bluesword/java/text/DateTimeFormatterTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/text/DateTimeFormatterTest.java
@@ -1,17 +1,24 @@
 package top.bluesword.java.text;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import java.time.*;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 import java.util.Locale;
 
+@Slf4j
 class DateTimeFormatterTest {
 
     @Test
     void format(){
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS ZZ").withZone(ZoneId.systemDefault());
-        System.out.println(dateTimeFormatter.format(Instant.now()));
+        log.info("format:{}",dateTimeFormatter.format(Instant.now()));
     }
 
     @Test
@@ -29,6 +36,20 @@ class DateTimeFormatterTest {
         DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.of("+00:00")).withLocale(Locale.CHINA);
         System.out.println(dateTimeFormatter0.format(instant));
         System.out.println(dateTimeFormatter1.format(instant));
+    }
+
+    @Test
+    void parse(){
+        DateTimeFormatter dateTimeFormatter = 
+                new DateTimeFormatterBuilder()
+                        .appendPattern("[yyyy][yy][-][/][MM][M][-][/][dd][d][ HH:mm]")
+                        .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+                        .toFormatter()
+                        .withZone(ZoneId.systemDefault());
+        log.info("parse 1 :{}",ZonedDateTime.from(dateTimeFormatter.parse("2022-12-09 15:29")));
+        log.info("parse 2 :{}",ZonedDateTime.from(dateTimeFormatter.parse("2022-12-09")));
+        log.info("parse 3 :{}",ZonedDateTime.from(dateTimeFormatter.parse("2022/12/09")));
+        log.info("parse 4 :{}",ZonedDateTime.from(dateTimeFormatter.parse("22/1/9")));
     }
 
 }
