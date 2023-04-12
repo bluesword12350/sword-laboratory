5dd41397828e60a488ce5528a277a1b3ebb7582e
李林峰 <yunlelongzai@qq.com>
2023-04-12 16:50:00 +0800

    货币类型测试

--- sword-laboratory/src/test/java/top/bluesword/java/util/CurrencyTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/util/CurrencyTest.java
@@ -1,15 +1,19 @@
 package top.bluesword.java.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.Currency;
 import java.util.Locale;
 
+@Slf4j
 public class CurrencyTest {
 
     @Test
     void test(){
         Currency currency = Currency.getInstance(Locale.getDefault());
-        System.out.println(currency.getSymbol());
+        log.info("currency.currencyCode:{}",currency.getCurrencyCode());
+        log.info("currency.symbol:{}",currency.getSymbol());
+        log.info("currency.getDisplayName(Locale.CHINA):{}",currency.getDisplayName(Locale.CHINA));
     }
 }
