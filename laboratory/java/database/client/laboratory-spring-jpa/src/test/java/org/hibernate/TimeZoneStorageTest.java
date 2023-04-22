64ab6b327c1d68fe2ad88f52a738b7fdd90c480b
李林峰 <yunlelongzai@qq.com>
2023-04-22 14:52:16 +0800

    hibernate 批处理测试

--- laboratory/java/database/client/laboratory-spring-jpa/src/test/java/org/hibernate/TimeZoneStorageTest.java
+++ laboratory/java/database/client/laboratory-spring-jpa/src/test/java/org/hibernate/TimeZoneStorageTest.java
@@ -4,8 +4,12 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import top.bluesword.laboratory.LaboratoryApplication;
+import top.bluesword.laboratory.model.Journey;
 import top.bluesword.laboratory.model.mock.JourneyMock;
 import top.bluesword.laboratory.repository.JourneyRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest(classes = LaboratoryApplication.class)
 public class TimeZoneStorageTest {
@@ -16,5 +20,14 @@ public class TimeZoneStorageTest {
     @Test
     void save(){
         journeyRepository.save(JourneyMock.mock());
+    }
+
+    @Test
+    void batchSave(){
+        List<Journey> journeyList = new ArrayList<>();
+        for (int i = 0; i < 100000; i++) {
+            journeyList.add(JourneyMock.mock());
+        }
+        journeyRepository.saveAll(journeyList);
     }
 }
