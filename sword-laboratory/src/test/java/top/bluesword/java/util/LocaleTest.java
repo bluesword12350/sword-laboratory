a13309f8609f7e347e2f7ed8570ca9d39f63f576
李林峰 <yunlelongzai@qq.com>
2023-05-26 14:26:22 +0800

    Locale 测试

--- sword-laboratory/src/test/java/top/bluesword/java/util/LocaleTest.java
+++ sword-laboratory/src/test/java/top/bluesword/java/util/LocaleTest.java
@@ -1,15 +1,18 @@
 package top.bluesword.java.util;
 
+import org.apache.commons.lang3.LocaleUtils;
 import org.junit.jupiter.api.Test;
 
 import java.util.Locale;
 
 class LocaleTest {
     @Test
-    void mainTest(){
-        Locale locale = new Locale("en","US");
-        System.out.println(locale.getDisplayCountry(Locale.US));
-        System.out.println(locale.getDisplayLanguage(Locale.US));
-        System.out.println(locale.getDisplayName(Locale.US));
+    void equals(){
+        Locale locale = Locale.SIMPLIFIED_CHINESE;
+        System.out.println(locale.equals(new Locale("zh_CN")));
+        System.out.println(locale.equals(new Locale("zh")));
+        System.out.println(locale.equals(new Locale("zh","cn")));
+        System.out.println(locale.equals(LocaleUtils.toLocale("zh_CN")));
     }
+
 }
