73af2719d3a26b3388efed23b0124d4b84fec73f
李林峰 <yunlelongzai@qq.com>
2023-06-03 17:49:06 +0800

    移除不再需要的代码

--- sword-laboratory/src/main/java/top/bluesword/Laboratory.java
+++ sword-laboratory/src/main/java/top/bluesword/Laboratory.java
@@ -4,9 +4,6 @@ package top.bluesword;
  * @author 李林峰
  */
 public class Laboratory {
-	public static void main(String[] args) {
-        String a = "Hello";
-        // \u000d\u000a a="world";
-        System.out.println(a);
-    }
+  public static void main(String[] args) {
+  }
 }
