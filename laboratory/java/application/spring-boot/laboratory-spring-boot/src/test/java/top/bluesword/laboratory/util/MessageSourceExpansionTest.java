454e103b7fcfdb373c6bb992e1fb0159f747de19
李林峰 <yunlelongzai@qq.com>
2023-07-24 08:15:08 +0800

    spring @EnabledIf（单元测试启用条件）使用测试

--- laboratory/java/application/spring-boot/laboratory-spring-boot/src/test/java/top/bluesword/laboratory/util/MessageSourceExpansionTest.java
+++ laboratory/java/application/spring-boot/laboratory-spring-boot/src/test/java/top/bluesword/laboratory/util/MessageSourceExpansionTest.java
@@ -4,11 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Locale;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
 
 @Slf4j
 @SpringBootTest
+@EnabledIf(expression = "#{'${spring.messages.basename}'=='demand-message'}",loadContext = true)
 class MessageSourceExpansionTest {
 
     @Autowired
