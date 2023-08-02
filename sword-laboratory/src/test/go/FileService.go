ad13898abc1d41f91ac2f9c8b40a90a78442a3dd
李林峰 <yunlelongzai@qq.com>
2023-08-02 14:28:17 +0800

    文件服务添加上传功能

--- sword-laboratory/src/test/go/FileService.go
+++ sword-laboratory/src/test/go/FileService.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	_ = os.Mkdir("file", os.ModePerm)
-	http.Handle("/file/", http.StripPrefix("/file/", http.FileServer(http.Dir("file"))))
-	err := http.ListenAndServe(":8080", nil)
+	pathName := "file"
+	_ = os.Mkdir(pathName, os.ModePerm)
+	fileUrl := "/file/"
+	http.Handle(fileUrl, http.StripPrefix(fileUrl, http.FileServer(http.Dir(pathName))))
+	http.HandleFunc("/upload", func(writer http.ResponseWriter, request *http.Request) {
+		file, fileHeader, _ := request.FormFile("upFile")
+		uploadPathName := pathName + "/share"
+		_ = os.Mkdir(uploadPathName, os.ModePerm)
+		saveFile, err := os.OpenFile(uploadPathName+"/"+fileHeader.Filename, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+		if err != nil {
+			log.Fatal("OpenFile error: ", err)
+		}
+		io.Copy(saveFile, file)
+	})
+	err := http.ListenAndServe(":9081", nil)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatal("ListenAndServe error : ", err)
 	}
 }
