4886af5d7ae287b9d31b39397fa4c39d4d6434cf
李林峰 <yunlelongzai@qq.com>
2023-08-03 10:00:42 +0800

    laboratory-element-plus 测试

---
+++ laboratory/javascript/component-library/laboratory-element-plus/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ElMenu}  from "element-plus";
+import {Setting} from '@element-plus/icons-vue'
+</script>
+
+<template>
+  <el-container>
+    <el-aside>
+      <el-menu>
+        <el-menu-item route="/timestamp">
+          <el-icon><setting /></el-icon>
+          <span>时间戳</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+</template>
+
+<style>
+</style>
---
+++ laboratory/javascript/component-library/laboratory-element-plus/src/components/Timestamp.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+const now = ref(Date.now());
+setInterval(() => now.value = Date.now(),1);
+</script>
+
+<template>
+    <h1 class="center-text">{{`${now} 毫秒`}}</h1>
+</template>
+
+<style scoped>
+    .center-text{
+        text-align:center;
+    }
+</style>
